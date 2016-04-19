07c2e094b9f49204bb3413a7448561b7d4881286
jimmy <504562194@qq.com>
2016-04-19 18:35:01 +0800

    mar

--- trade-web/src/main/java/com/centaline/trans/kpiImport/web/KpiImportController.java
+++ trade-web/src/main/java/com/centaline/trans/kpiImport/web/KpiImportController.java
@@ -1,11 +1,32 @@
 package com.centaline.trans.kpiImport.web;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.compress.archivers.dump.InvalidFormatException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.aist.common.utils.excel.ImportExcel;
+import com.aist.uam.auth.remote.UamSessionService;
+import com.aist.uam.auth.remote.vo.SessionUser;
+import com.aist.uam.userorg.remote.UamUserOrgService;
+import com.centaline.trans.kpi.vo.KpiMonthVO;
 
 @Controller
-@RequestMapping(value = "kpi")
+@RequestMapping(value = "kpi") 
 public class KpiImportController {
+	@Autowired(required = true)
+	UamSessionService uamSessionService;
+	@Autowired
+	private UamUserOrgService uamUserOrgService;
+	
 	@RequestMapping(value = "/import")
 	public String kpiImport() {
 		return "kpi/kpiInport";
@@ -14,5 +35,23 @@ public class KpiImportController {
 	@RequestMapping(value = "/doImport")
 	public String doKpiImport() {
 		return "kpi/kpiImport";
+	}
+	
+	@RequestMapping(value = "/monthKpiImport")
+	public String monthKpiImport() {
+		return "kpi/monthKpiImport";
+	}
+
+	@RequestMapping(value = "/doMonthKpiImport")
+	public String doMonthKpiImport(@RequestParam("fileupload") MultipartFile file, HttpServletRequest request,
+			HttpServletResponse response) throws InvalidFormatException, IOException, InstantiationException, IllegalAccessException {
+		ImportExcel ie = new ImportExcel(file, 0, 0);
+		List<KpiMonthVO> list = ie.getDataList(KpiMonthVO.class);
+		
+		uamUserOrgService.getUserByEmployeeCode("");
+		
+		//uamUserOrgService.getUserOrgJobByUserIdAndJobCode(arg0, arg1)
+		
+		return "kpi/monthKpiImport";
 	}
 }
