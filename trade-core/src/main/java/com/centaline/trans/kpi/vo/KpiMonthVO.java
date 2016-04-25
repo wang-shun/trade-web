368bb2c3cdda391ee5498b7351bfe20bead6a801
huxxx <491037720@qq.com>
2016-04-25 15:22:54 +0800

    修改kpi导入的bug

--- trade-core/src/main/java/com/centaline/trans/kpi/vo/KpiMonthVO.java
+++ trade-core/src/main/java/com/centaline/trans/kpi/vo/KpiMonthVO.java
@@ -1,7 +1,5 @@
 package com.centaline.trans.kpi.vo;
 
-import java.util.Date;
-
 import com.aist.common.utils.excel.annotation.ExcelField;
 
 public class KpiMonthVO {
@@ -9,14 +7,14 @@ public class KpiMonthVO {
 	private String userName;
 	@ExcelField(title = "员工编号")
 	private String employeeCode;
-	@ExcelField(title = "所在组织")
+	/*@ExcelField(title = "所在组织")
 	private String orgName;
 	@ExcelField(title = "岗位")
-	private String jobName;
+	private String jobName;*/
 	@ExcelField(title = "当月金融产品完成单数(包含外单)")
 	private Long finOrder;
-	@ExcelField(title = "归属月份")
-	private Date belongMonth;
+	/*@ExcelField(title = "归属月份")
+	private Date belongMonth;*/
 	
 	private String errorMessage;
 
@@ -36,36 +34,12 @@ public class KpiMonthVO {
 		this.userName = userName;
 	}
 
-	public String getJobName() {
-		return jobName;
-	}
-
-	public void setJobName(String jobName) {
-		this.jobName = jobName;
-	}
-
-	public String getOrgName() {
-		return orgName;
-	}
-
-	public void setOrgName(String orgName) {
-		this.orgName = orgName;
-	}
-
 	public Long getFinOrder() {
 		return finOrder;
 	}
 
 	public void setFinOrder(Long finOrder) {
 		this.finOrder = finOrder;
-	}
-
-	public Date getBelongMonth() {
-		return belongMonth;
-	}
-
-	public void setBelongMonth(Date belongMonth) {
-		this.belongMonth = belongMonth;
 	}
 
 	public String getErrorMessage() {
