c71b8137be2e96ab90521810207be0a47dc818d0
huxxx <491037720@qq.com>
2016-05-05 20:14:51 +0800

    修改字符的bug

--- trade-core/src/main/java/com/centaline/trans/remote/service/impl/EguServiceImpl.java
+++ trade-core/src/main/java/com/centaline/trans/remote/service/impl/EguServiceImpl.java
@@ -359,8 +359,6 @@ public class EguServiceImpl implements EguService {
 
 		String token = SignUtil.buildRequestToken(paramMap, Const.TOKEN);
 		String url = code+"/disagree?token="+token+"&"+SignUtil.createLinkString(paramMap);
-		url = url.replaceAll("&", "%26");
-		url = url.replaceAll(" ", "%20");
 		HttpResponse httpResponse = executeGet(url);
 		HttpEntity entity = httpResponse.getEntity();
 		String returnStr = EntityUtils.toString(entity, "UTF-8");
@@ -489,7 +487,6 @@ public class EguServiceImpl implements EguService {
 
 			String token = SignUtil.buildRequestToken(paramMap, Const.TOKEN);
 			String url = evaCode +"/upload?token=" + token +"&"+ SignUtil.createLinkString(paramMap);
-			url = url.replaceAll("&", "%26");
 			url = url.replaceAll(" ", "%20");
 			HttpResponse httpResponse = executeGet(url);
 
