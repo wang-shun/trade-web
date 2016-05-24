6f9bf041f5b2391fef4a0c154d230b487f946d1e
jimmy <504562194@qq.com>
2016-05-24 09:58:26 +0800

    产调数据结构相关修改

--- trade-core/src/main/java/com/centaline/trans/property/service/impl/ToPropertyResarchServiceImpl.java
+++ trade-core/src/main/java/com/centaline/trans/property/service/impl/ToPropertyResarchServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.aist.common.exception.BusinessException;
@@ -13,19 +14,20 @@ import com.aist.uam.basedata.remote.UamBasedataService;
 import com.aist.uam.userorg.remote.UamUserOrgService;
 import com.aist.uam.userorg.remote.vo.Org;
 import com.aist.uam.userorg.remote.vo.User;
-import com.centaline.trans.cases.service.ToCaseService;
+import com.aist.uam.userorg.remote.vo.UserOrgJob;
 import com.centaline.trans.common.entity.ToPropertyInfo;
 import com.centaline.trans.common.enums.PrChannelEnum;
 import com.centaline.trans.common.enums.PropertyStatusEnum;
 import com.centaline.trans.common.enums.ToPropertyResearchEnum;
 import com.centaline.trans.common.enums.TransJobs;
-import com.centaline.trans.common.service.ToPropertyInfoService;
 import com.centaline.trans.property.service.ToPropertyResearchService;
 import com.centaline.trans.task.entity.ToPropertyResearch;
 import com.centaline.trans.task.entity.ToPropertyResearchVo;
 import com.centaline.trans.task.entity.TsPrResearchMap;
 import com.centaline.trans.task.repository.ToPropertyResearchMapper;
 import com.centaline.trans.task.repository.TsPrResearchMapMapper;
+
+import sun.management.resources.agent;
 
 @Service
 public class ToPropertyResarchServiceImpl implements ToPropertyResearchService {
@@ -36,24 +38,78 @@ public class ToPropertyResarchServiceImpl implements ToPropertyResearchService {
 	@Autowired
 	private TsPrResearchMapMapper tsPrResearchMapper;
 	@Autowired
-	private ToCaseService caseService;
-	@Autowired
-	private ToPropertyInfoService propertyInfoService;
-	@Autowired
 	private UamBasedataService uamBasedataService;
-
+	@Value("${centaline.environment}")
+	private String environment;
+	@Override
+	public String getEnvironment(){
+		return this.environment;
+	}
 	@Override
 	public int recordProperty(ToPropertyResearchVo vo) {
+		if (StringUtils.isBlank(vo.getPrChannel())) {
+			getChannel(vo);
+		}
+		if (PrChannelEnum.TJJR.getCode().equals(vo.getPrChannel())) {//经纪人发起 根据行政区号获得大区ID
+			getDistrictId(vo);
+		}
 
-		String caseCode = uamBasedataService.nextSeqVal("CHANDIAO_CODE", new Date());
-		vo.setCaseCode(caseCode);
+		if (StringUtils.isBlank(vo.getAppliant())) {
+			User user = uamUserOrgService.getUserByUsername(vo.getUsername());
+			if (user == null) {
+				throw new BusinessException("用户名不存在:" + vo.getUsername());
+			}
+			vo.setAppliant(user.getId());
+		}
+		if (StringUtils.isBlank(vo.getPrCostOrgId())) {
+			if (PrChannelEnum.TJJR.getCode().equals(vo.getPrChannel())) {
+
+				setPrCost(vo, vo.getAppliant());
+			} else {
+				setPrCost(vo, vo.getAgentCode());
+			}
+		}
+
+		String prCode = uamBasedataService.nextSeqVal("CHANDIAO_CODE", new Date());
+		vo.setPrCode(prCode);
 		ToPropertyResearch tpr = buildPrResearch(vo);
-		vo.setDistrictId(tpr.getPrDistrictId());
+		return mapper.insertSelective(tpr);
+	}
 
-		ToPropertyInfo pi = buildPropertyInfo(vo);
-		mapper.insertSelective(tpr);
-		propertyInfoService.insertSelective(pi);
-		return 1;
+	private void setPrCost(ToPropertyResearchVo vo, String userId) {
+		String orgId = mapper.getOrgIdByUserId(userId);
+		Org org=null;
+		if(orgId!=null){
+			org=uamUserOrgService.getOrgById(orgId);
+		}
+		if(org!=null){
+		vo.setPrCostOrgId(org.getId());
+		vo.setPrCostOrgName(org.getOrgName());
+		vo.setPrCostOrgMgr(mapper.getPrCostMgrByOrgId(org.getId()));
+		}
+	}
+
+
+	private void getChannel(ToPropertyResearchVo vo) {
+		if (StringUtils.isBlank(vo.getCaseCode())) {
+			vo.setPrChannel(PrChannelEnum.TJJR.getCode());
+		} else {
+			vo.setPrChannel(PrChannelEnum.TJYGW.getCode());
+		}
+	}
+
+	private void getDistrictId(ToPropertyResearchVo vo) {
+		List<TsPrResearchMap> researchMaps = tsPrResearchMapper.selectByDistCode(vo.getDistrict());
+		TsPrResearchMap rm = null;
+		if (researchMaps != null && !researchMaps.isEmpty()) {
+			rm = researchMaps.get(0);
+		}
+		if (rm != null && !StringUtils.isBlank(rm.getYuDistCode())) {
+			Org org = uamUserOrgService.getOrgByCode(rm.getYuDistCode());
+			if (org != null) {
+				vo.setDistrictId(org.getId());
+			}
+		}
 	}
 
 	/**
@@ -87,14 +143,6 @@ public class ToPropertyResarchServiceImpl implements ToPropertyResearchService {
 		return null;
 	}
 
-	public ToPropertyInfo buildPropertyInfo(ToPropertyResearchVo vo) {
-		ToPropertyInfo pi = new ToPropertyInfo();
-		pi.setCaseCode(vo.getCaseCode());
-		pi.setDistCode(vo.getDistrict());
-		pi.setPropertyAddr(vo.getPropertyAddr());
-		return pi;
-	}
-
 	@Override
 	public boolean hasMapping(String districtCode) {
 		List<TsPrResearchMap> researchMaps = tsPrResearchMapper.selectByDistCode(districtCode);
@@ -105,45 +153,32 @@ public class ToPropertyResarchServiceImpl implements ToPropertyResearchService {
 	public ToPropertyResearch buildPrResearch(ToPropertyResearchVo vo) {
 		ToPropertyResearch record = new ToPropertyResearch();
 
-		User user = uamUserOrgService.getUserByUsername(vo.getUsername());
-		ToPropertyResearch sameOne = mapper.selectByForSamePrCode(vo.getPropertyAddr());
+		ToPropertyResearch sameOne = mapper.selectByForSamePrCode(vo.getPropertyAddr(),vo.getPrCat());
 		if (sameOne != null) {
 			record.setSamePRCode(sameOne.getPrCode());
 		}
-		if (user == null) {
-			throw new BusinessException("用户名不存在:" + vo.getUsername());
-		}
 
-		String appliant = user.getId();
-
-		record.setPrCode(vo.getCaseCode());
-		record.setPrAppliant(appliant);
+		record.setPrCode(vo.getPrCode());
+		record.setCaseCode(vo.getCaseCode());
+		record.setPrAppliant(vo.getAppliant());
 		record.setPartCode(ToPropertyResearchEnum.PROPERTY_RESEARCH.getCode());
 		record.setPrCat(vo.getPrCat());
-		record.setPrChannel(PrChannelEnum.TJJR.getCode());
-		record.setPrStatus(PropertyStatusEnum.CONTACTS.getCode());
+		record.setPrStatus(vo.getPrChannel());
 		record.setPrApplyTime(new Date());
-		List<TsPrResearchMap> researchMaps = tsPrResearchMapper.selectByDistCode(vo.getDistrict());
-		TsPrResearchMap rm = null;
-		if (researchMaps != null && !researchMaps.isEmpty()) {
-			rm = researchMaps.get(0);
-		}
-		if (rm != null && !StringUtils.isBlank(rm.getYuDistCode())) {
-			Org org = uamUserOrgService.getOrgByCode(rm.getYuDistCode());
-			// 秘书 优先 每个组的助理
-			//
-			if (org != null) {
-				record.setPrDistrictId(org.getId());
-				/*
-				 * List<User> users =
-				 * uamUserOrgService.getUserByOrgIdAndJobCode( org.getId(),
-				 * TransJobs.TQYMS.getCode()); if (users != null &&
-				 * !users.isEmpty()) { User u = users.get(0);
-				 * 
-				 * record.setPrExecutor(u.getId() + ""); }
-				 */
-			}
-		}
+
+		record.setDistCode(vo.getDistrict());
+		record.setPrAddress(vo.getPropertyAddr());
+		record.setCreateTime(new Date());
+		record.setCreateBy(vo.getAppliant());
+		record.setPrDistrictId(vo.getDistrictId());
+
+		record.setPrApplyOrgId(vo.getPrApplyOrgId());
+		record.setPrApplyOrgName(vo.getPrApplyOrgName());
+		record.setPrApplyTime(new Date());
+
+		record.setPrCostOrgId(vo.getPrCostOrgId());
+		record.setPrCostOrgMgr(vo.getPrCostOrgMgr());
+		record.setPrCostOrgName(vo.getPrCostOrgName());
 		return record;
 	}
 
