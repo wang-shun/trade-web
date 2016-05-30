ba23f33f2bc2a25b6f3f2cf07b1b3e90e0e03889
maximum-wong <maximum2012@qq.com>
2016-05-30 09:50:32 +0800

    commit

--- trade-web/src/main/java/com/centaline/trans/report/web/ReportCaseProcessStatisController.java
+++ trade-web/src/main/java/com/centaline/trans/report/web/ReportCaseProcessStatisController.java
@@ -41,11 +41,16 @@ public class ReportCaseProcessStatisController {
 	 * @param model
 	 * @param request
 	 * @param status
+	 * @param orgs
+	 * @param orgName
+	 * @param createTimeStart
+	 * @param createTimeEnd
 	 * @return
 	 */
-	@SuppressWarnings("deprecation")
 	@RequestMapping(value = "caseDetail")
-	public String caseDetail(Model model, ServletRequest request,
+	public String caseDetail(
+			Model model,
+			ServletRequest request,
 			@RequestParam(value = "status", required = false) String status,
 			@RequestParam(value = "orgs", required = false) String orgs,
 			@RequestParam(value = "orgName", required = false) String orgName,
@@ -57,12 +62,50 @@ public class ReportCaseProcessStatisController {
 		request.setAttribute("orgs", orgs);
 		request.setAttribute("orgName", orgName);
 
-		if(!"signed".equals(status)&&!"transfered".equals(status)&&"closed".equals(status)){
+		if (!"signed".equals(status) && !"transfered".equals(status)
+				&& "closed".equals(status)) {
 			status = "received";
 		}
 		request.setAttribute("status", status);
 
 		return "report/case_detail";
+	}
+
+	/**
+	 * 案件状态基本统计
+	 * 
+	 * @param model
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "caseQuery")
+	public String caseQuery(Model model, ServletRequest request) {
+		SessionUser user = uamSessionService.getSessionUser();
+		String orgs = null;
+		String org = null;
+		String userId = null;
+		String districtId = null;
+
+		if (TransJobs.TZJL.getCode().equals(user.getServiceJobCode())) {// 如果是总经理
+			List<Org> orgList = uamUserOrgService.getOrgByParentId(null);
+			orgs = orgListToListStr(orgList);
+		} else if (TransJobs.TZJ.getCode().equals(user.getServiceJobCode())) {// 如果是总监
+			districtId =user.getServiceDepId();
+			List<Org> orgList = uamUserOrgService.getOrgByParentId(districtId);
+			orgs = orgListToListStr(orgList);
+		} else if (TransJobs.TSJYZG.getCode().equals(user.getServiceJobCode())
+				|| TransJobs.TJYZG.getCode().equals(user.getServiceJobCode())) {// 如果是交易主管
+			org = user.getServiceDepId();
+		} else {// 交易顾问
+			userId = user.getId();
+		}
+	    String month =(new Date().getMonth()+1)+"";
+		request.setAttribute("orgs", orgs);
+		request.setAttribute("orgs", orgs);
+		request.setAttribute("userId", userId);
+		request.setAttribute("districtId", districtId);
+		request.setAttribute("month", month);
+		return "report/case_query_count";
 	}
 
 //	private Map<String, String> buildWorkSpaceBean(String serachId) {
@@ -77,7 +120,8 @@ public class ReportCaseProcessStatisController {
 //						.getOrgByParentId(serachId);
 //				orgs = orgListToListStr(orgList);
 //			}
-//		} else if (TransJobs.TZJ.getCode().equals(user.getServiceJobCode())) { // 如果是总监
+//		} else if (TransJobs.TZJ.getCode().equals(user.getServiceJobCode())) { //
+//			// 如果是总监
 //			if (!StringUtils.isBlank(serachId)) {
 //				org = serachId;
 //			} else {
@@ -86,13 +130,15 @@ public class ReportCaseProcessStatisController {
 //				orgs = orgListToListStr(orgList);
 //			}
 //		} else if (TransJobs.TSJYZG.getCode().equals(user.getServiceJobCode())
-//				|| TransJobs.TJYZG.getCode().equals(user.getServiceJobCode())) {// 如果是交易主管
+//				|| TransJobs.TJYZG.getCode().equals(user.getServiceJobCode())) {//
+//			// 如果是交易主管
 //			if (!StringUtils.isBlank(serachId)) {
 //				userId = serachId;
 //			} else {
 //				org = user.getServiceDepId();
 //			}
 //		} else {
+//			// 交易顾问
 //			userId = user.getId();
 //		}
 //		map.put("userId", userId);
@@ -100,16 +146,16 @@ public class ReportCaseProcessStatisController {
 //		map.put("orgs", orgs);
 //		return map;
 //	}
-//
-//	private String orgListToListStr(List<Org> orgs) {
-//		StringBuffer sb = new StringBuffer();
-//		if (orgs == null || orgs.isEmpty())
-//			return null;
-//		for (Org org : orgs) {
-//			sb.append(org.getId() + ",");
-//		}
-//		String strOrgs = sb.toString();
-//		strOrgs.substring(0, strOrgs.length() - 1);
-//		return strOrgs;
-//	}
+
+	private String orgListToListStr(List<Org> orgs) {
+		StringBuffer sb = new StringBuffer();
+		if (orgs == null || orgs.isEmpty())
+			return null;
+		for (Org org : orgs) {
+			sb.append(org.getId() + ",");
+		}
+		String strOrgs = sb.toString();
+		strOrgs.substring(0, strOrgs.length() - 1);
+		return strOrgs;
+	}
 }
