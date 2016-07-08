c1c6dcc5983ee2cd198b97466299d43969c69c70
zhoujp7 <zhoujp7@SHCYF62T.centaline.com.cn>
2016-07-08 17:01:58 +0800

    统计报表: 1、产调导出 2、过户案件 3、红绿灯任务报表

---
+++ trade-web/src/main/webapp/static/js/trunk/report/redgreen_task_count.js
@@ -0,0 +1,188 @@
+$(document).ready(function() {
+					cleanForm();
+					//基本信息等高
+					var url = "/quickGrid/findPage";
+					var ctx = $("#ctx").val();
+					url = ctx + url;
+					var queryOrgFlag = $("#queryOrgFlag").val();
+					var isAdminFlag = $("#isAdminFlag").val();
+					var queryOrgs = $("#queryOrgs").val();
+					var arguUserId=null;
+					if(queryOrgFlag == 'true'){
+						arguUserId=null;
+						if(isAdminFlag == 'true'){
+							queryOrgs=null;
+						}
+					}else{
+						queryOrgs= null;
+						arguUserId="yes";
+					}
+
+					var orgArray = queryOrgs==null?null:queryOrgs.split(",");
+					// 初始化列表
+					var data = {};
+		    	    data.queryId = "queryRedGreenTaskCountList";
+		    	    data.rows = 12;
+		    	    data.page = 1;
+		    		reloadGrid(data);
+				
+				});
+
+// select控件
+var config = {
+	'.chosen-select' : {},
+	'.chosen-select-deselect' : {
+		allow_single_deselect : true
+	},
+	'.chosen-select-no-single' : {
+		disable_search_threshold : 10
+	},
+	'.chosen-select-no-results' : {
+		no_results_text : 'Oops, nothing found!'
+	},
+	'.chosen-select-width' : {
+		width : "95%"
+	}
+};
+
+for ( var selector in config) {
+	$(selector).chosen(config[selector]);
+};
+
+// 日期控件
+$('#datepicker_0').datepicker({
+	format : 'yyyy-mm-dd',
+	weekStart : 1,
+	autoclose : true,
+	todayBtn : 'linked',
+	language : 'zh-CN'
+});
+
+// 查询
+$('#searchButton').click(function() {
+	searchMethod();
+});
+
+ //查询
+function searchMethod(page) {
+	if(!page) {
+		page = 1;
+	}
+	
+	var params = getParamsValue();
+	params.page = page;
+	params.rows = 12;
+	params.queryId = "queryRedGreenTaskCountList";
+	reloadGrid(params);
+	
+
+};
+
+function reloadGrid(data) {
+	var queryOrgFlag = $("#queryOrgFlag").val();
+	var isAdminFlag = $("#isAdminFlag").val();
+	var queryOrgs = $("#queryOrgs").val();
+	var arguUserId=null;
+	if(queryOrgFlag == 'true'){
+		arguUserId=null;
+		if(isAdminFlag == 'true'){
+			queryOrgs=null;
+		}
+	}else{
+		queryOrgs= null;
+		arguUserId="yes";
+	}
+
+	var orgArray = queryOrgs==null?null:queryOrgs.split(",");
+	data.argu_idflag = arguUserId;
+    data.argu_queryorgs = orgArray;
+	
+	$.ajax({
+		async: true,
+        url:ctx+ "/quickGrid/findPage" ,
+        method: "post",
+        dataType: "json",
+        data: data,
+        beforeSend: function () {  
+        	$.blockUI({message:$("#salesLoading"),css:{'border':'none','z-index':'9999'}}); 
+			$(".blockOverlay").css({'z-index':'9998'});
+        },  
+        success: function(data){
+          $.unblockUI();   	 
+      	  data.ctx = ctx;
+      	  var redgreenTaskList = template('template_redgreenTaskList' , data);
+			  $("#redgreenTaskList").empty();
+			  $("#redgreenTaskList").html(redgreenTaskList);
+			  // 显示分页 
+            initpage(data.total,data.pagesize,data.page, data.records);
+        },
+        error: function (e, jqxhr, settings, exception) {
+        	$.unblockUI();   	 
+        }  
+  });
+}
+
+
+
+function initpage(totalCount,pageSize,currentPage,records)
+{
+	if(totalCount>1500){
+		totalCount = 1500;
+	}
+	var currentTotalstrong=$('#currentTotalPage').find('strong');
+	if (totalCount<1 || pageSize<1 || currentPage<1)
+	{
+		$(currentTotalstrong).empty();
+		$('#totalP').text(0);
+		$("#pageBar").empty();
+		return;
+	}
+	$(currentTotalstrong).empty();
+	$(currentTotalstrong).text(currentPage+'/'+totalCount);
+	$('#totalP').text(records);
+	
+	
+	$("#pageBar").twbsPagination({
+		totalPages:totalCount,
+		visiblePages:9,
+		startPage:currentPage,
+		first:'<i class="icon-step-backward"></i>',
+		prev:'<i class="icon-chevron-left"></i>',
+		next:'<i class="icon-chevron-right"></i>',
+		last:'<i class="icon-step-forward"></i>',
+		showGoto:true,
+		onPageClick: function (event, page) {
+			 //console.log(page);
+			searchMethod(page);
+	    }
+	});
+}
+
+
+function getParamsValue() {
+	
+	var start = $('#dtBegin_0').val();
+	//设置查询参数
+	var params = {
+			prApplyTime : start
+	};
+	return params;
+}
+
+
+function queryRedGreenTaskDetail(id){
+	//var start = $('#dtBegin_0').val();
+	window.open(ctx+"/report/redgreenTaskDetail?organId="+id);
+	
+}
+
+// 清空表单
+function cleanForm() {
+	$("input[name='dtBegin']").val("");
+}
+
+//清空
+$('#cleanButton').click(function() {
+	$("input[name='dtBegin']").val('');
+	$("select").val("");
+});
