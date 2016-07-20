0f7b771973b9826f42ef6356011eb5dfb4bd86ac
huxxx <491037720@qq.com>
2016-07-20 12:10:57 +0800

    修改流程的bug

--- trade-core/src/main/java/com/centaline/trans/common/enums/WorkFlowEnum.java
+++ trade-core/src/main/java/com/centaline/trans/common/enums/WorkFlowEnum.java
@@ -9,7 +9,7 @@ public enum WorkFlowEnum {
 			"OfflineEva", "businessKey"),SPV_OUT("spv_out","businessKey"),SERVICE_RESTART("serviceRestart","serviceRestartBusinesskey"),
 	        COMLOAN_PROCESS("ComLoan_Process:4:645463","ComLoan_Process"),
 	        LOANLOST_PROCESS("LoanLost_Process:1:641492","LoanLost_Process"),
-	        PSFLOAN_PROCESS("PSFLoan_Process:1:645460","LoanLost_Process"),
+	        PSFLOAN_PROCESS("PSFLoan_Process:1:645460","PSFLoan_Process"),
 	        OPERATION_PROCESS("operation_process:40:645454","processDefinitionId");
 	private String name;
 	private String code;
