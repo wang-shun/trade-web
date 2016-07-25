2e044bee650c43b1efbca369e7a1902a072d922c
gongjieduo <gongjd@centaline.com.cn>
2016-07-25 20:07:40 +0800

    无法更改临时银行选择
    Change-Id: I918a894121684c51f2a3976f0c8d651daf904266
    Signed-off-by: gongjieduo <gongjd@centaline.com.cn>

--- trade-web/src/main/java/com/centaline/trans/taskList/web/TaskController.java
+++ trade-web/src/main/java/com/centaline/trans/taskList/web/TaskController.java
@@ -304,6 +304,8 @@ public class TaskController {
         		request.setAttribute("evaCode", "");
     		}
     		
+    		request.setAttribute("readonly_", "readonly='readonly'");
+    		
     	} else if(taskitem.equals("LoanlostApply")){/*贷款流失申请*/
     		getAccesoryList(request, taskitem);
     		/**这里应该和自办贷款一样*/
