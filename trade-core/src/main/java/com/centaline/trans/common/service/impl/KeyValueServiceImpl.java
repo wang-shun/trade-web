767660e05014dfa79a4ddb6507780c135666328e
zhuody <zhuody@centaline.com.cn>
2016-08-05 17:40:38 +0800

    注释

--- trade-core/src/main/java/com/centaline/trans/common/service/impl/KeyValueServiceImpl.java
+++ trade-core/src/main/java/com/centaline/trans/common/service/impl/KeyValueServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.centaline.trans.common.repository.KeyValueMapper;
@@ -150,7 +149,6 @@ public class KeyValueServiceImpl implements KeyValueService {
 						}
 					}
 				}
-
 			}
 		}
 	}
