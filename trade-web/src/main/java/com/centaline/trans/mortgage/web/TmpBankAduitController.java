d3abf6fc4ccdadb7a98136d7bc32598dc379f88f
gongjieduo <gongjd@centaline.com.cn>
2016-08-10 10:28:38 +0800

    金额*10000
    Change-Id: I61f92a004e6439d176f6165e9010177cc5646bdf
    Signed-off-by: gongjieduo <gongjd@centaline.com.cn>

--- trade-web/src/main/java/com/centaline/trans/mortgage/web/TmpBankAduitController.java
+++ trade-web/src/main/java/com/centaline/trans/mortgage/web/TmpBankAduitController.java
@@ -123,8 +123,13 @@ public class TmpBankAduitController {
 	
 	//更新贷款表临时银行状态为审批中：‘2’
 	ToMortgage mortageDb = toMortgageService.findToMortgageByCaseCode2(caseCode);
-	mortageDb.setTmpBankStatus("2");
-	toMortgageService.updateToMortgage(mortageDb);
+	if(mortageDb != null){
+		mortageDb.setComAmount(mortageDb.getComAmount() != null?mortageDb.getComAmount().multiply(new BigDecimal(10000)):null);
+		mortageDb.setMortTotalAmount(mortageDb.getMortTotalAmount() != null?mortageDb.getMortTotalAmount().multiply(new BigDecimal(10000)):null);
+		mortageDb.setPrfAmount(mortageDb.getPrfAmount() != null?mortageDb.getPrfAmount().multiply(new BigDecimal(10000)):null);
+		mortageDb.setTmpBankStatus("2");
+		toMortgageService.updateToMortgage(mortageDb);
+	}
 	
 	return vo;
 	}
