1732663f0ac3ffd1edd0d6dd3a37cf1e10a7d1d2
huxxx <491037720@qq.com>
2016-08-19 11:34:29 +0800

    增加部署流程定义ID

--- trade-core/src/main/java/com/centaline/trans/common/service/impl/PropertyUtilsServiceImpl.java
+++ trade-core/src/main/java/com/centaline/trans/common/service/impl/PropertyUtilsServiceImpl.java
@@ -52,7 +52,13 @@ public class PropertyUtilsServiceImpl implements PropertyUtilsService {
 	
 	@Value("${process.df.key.Eloan_Process}")
 	private String processEloanDfKey;
-
+	
+	@Value("${process.df.key.Spv_Process}")
+	private String spvProcess;
+	
+	public String getSpvProcess() {
+		return spvProcess;
+	}
 	public String getProcessTmpBankAuditDfKey() {
 		return processTmpBankAuditDfKey;
 	}
