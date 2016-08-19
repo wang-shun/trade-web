27d5cfc4582579513f79ed4bda1180a0cda55459
gongjieduo <gongjd@centaline.com.cn>
2016-08-19 10:10:36 +0800

    贷款信息列表
    Change-Id: Ie8b0a6c9dd91cc783d67b926a7369674c63cfb31
    Signed-off-by: gongjieduo <gongjd@centaline.com.cn>

---
+++ trade-web/src/main/java/com/centaline/trans/mortgage/web/MortgageInfoListController.java
@@ -0,0 +1,90 @@
+package com.centaline.trans.mortgage.web;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.servlet.ServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.aist.uam.auth.remote.UamSessionService;
+import com.aist.uam.auth.remote.vo.SessionUser;
+import com.aist.uam.userorg.remote.UamUserOrgService;
+import com.aist.uam.userorg.remote.vo.Org;
+import com.centaline.trans.common.enums.DepTypeEnum;
+import com.centaline.trans.common.enums.TransJobs;
+import com.centaline.trans.mortgage.service.ToMortgageService;
+
+@Controller
+@RequestMapping(value="mortgageInfo")
+public class MortgageInfoListController {
+
+	@Autowired
+	ToMortgageService toMortgageService;
+	
+	@Autowired(required = true)
+	UamSessionService uamSessionService;
+	
+	@Autowired(required = true)
+	UamUserOrgService uamUserOrgService;
+	
+	@RequestMapping(value = "/list")
+	public String list(ServletRequest request) {
+		SessionUser user = uamSessionService.getSessionUser();
+		String userJob=user.getServiceJobCode();
+		boolean queryOrgFlag = false;
+		boolean isAdminFlag = false;
+
+        StringBuffer reBuffer = new StringBuffer();
+		if(!userJob.equals(TransJobs.TJYGW.getCode())){
+			queryOrgFlag=true;
+			String depString = user.getServiceDepHierarchy();
+			String userOrgIdString = user.getServiceDepId();
+			if(depString.equals(DepTypeEnum.TYCTEAM.getCode())){
+				reBuffer.append(userOrgIdString);
+			}else if(depString.equals(DepTypeEnum.TYCQY.getCode())){
+				List<Org> orgList = uamUserOrgService.getOrgByDepHierarchy(userOrgIdString, DepTypeEnum.TYCTEAM.getCode());
+				for(Org org:orgList){
+					reBuffer.append(org.getId());
+					reBuffer.append(",");
+				}
+				reBuffer.deleteCharAt(reBuffer.length()-1);
+				
+			}else{
+				isAdminFlag=true;
+			}
+		}
+		request.setAttribute("queryOrgs", reBuffer.toString());
+		request.setAttribute("queryOrgFlag", queryOrgFlag);
+		request.setAttribute("isAdminFlag", isAdminFlag);	
+		
+		//默认显示上周数据
+		Calendar c1 = Calendar.getInstance();
+		Calendar c2 = Calendar.getInstance();
+		
+		c1.set(Calendar.DAY_OF_MONTH, 1); 
+		c1.add(Calendar.MONTH,-1);
+		
+		c2.set(Calendar.DAY_OF_MONTH, 0); 
+
+		String start = new SimpleDateFormat("yyyy-MM-dd").format(c1.getTime());//last Monday
+		String end = new SimpleDateFormat("yyyy-MM-dd").format(c2.getTime());//last Sunday
+		
+		String signTimeStart = request.getParameter("signTimeStart");
+		String signTimeEnd = request.getParameter("signTimeEnd");
+
+		if(StringUtils.isEmpty(signTimeStart) && StringUtils.isEmpty(signTimeEnd)){
+			signTimeStart = start;
+			signTimeEnd = end;
+		}
+
+		request.setAttribute("signTimeStart", signTimeStart);
+		request.setAttribute("signTimeEnd", signTimeEnd);
+		
+		return "mortgage/mortgageInfoList";		
+	}
+}
