ef970dc72b0431dc19922fee4025eed18637ebf9
aisyuzhong <211735722@qq.com>
2016-09-05 09:59:36 +0800

    九宫格上传

---
+++ trade-demo/UI页面调整工作/adminHtml/static/trans/js/eloan/eloan_guarant.js
@@ -0,0 +1,68 @@
+
+$(function() {
+
+/*开关按钮*/
+  $('[name="status"]').bootstrapSwitch({
+      onText:"已修改",
+      offText:"未修改",
+      onColor:"primary",
+      offColor:"default",
+      size:"small",
+      onSwitchChange:function(event,state){
+          if(state==true){
+              $(this).val("1");
+          }else{
+              $(this).val("2");
+          }
+      }
+  })
+
+
+  /*poshytip 提示框JS*/
+  //left
+    $('.demo-left').poshytip({
+      className: 'tip-twitter',
+      showTimeout: 1,
+      alignTo: 'target',
+      alignX: 'left',
+      alignY: 'center',
+      offsetX: 8,
+      offsetY: 5,
+    });
+
+    //right
+    $('.demo-right').poshytip({
+      className: 'tip-twitter',
+      showTimeout: 1,
+      alignTo: 'target',
+      alignX: 'right',
+      alignY: 'center',
+      offsetX: 8,
+      offsetY: 5,
+    });
+
+    //top
+    $('.demo-top').poshytip({
+      className: 'tip-twitter',
+      showTimeout: 1,
+      alignTo: 'target',
+      alignX: 'center',
+      alignY: 'top',
+      offsetX: 8,
+      offsetY: 5,
+    });
+
+    //bottom
+    $('.demo-bottom').poshytip({
+      className: 'tip-twitter',
+      showTimeout: 1,
+      alignTo: 'target',
+      alignX: 'center',
+      alignY: 'bottom',
+      offsetX: 8,
+      offsetY: 5,
+    });
+
+
+})
+
