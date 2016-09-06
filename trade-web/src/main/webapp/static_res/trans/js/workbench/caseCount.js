3b94c53409228fb1939245029116cc68e90238ce
zhoujp7 <zhoujp7@SHCYF62T.centaline.com.cn>
2016-09-06 08:41:17 +0800

    九宫格

--- trade-web/src/main/webapp/static_res/trans/js/workbench/caseCount.js
+++ trade-web/src/main/webapp/static_res/trans/js/workbench/caseCount.js
@@ -8,36 +8,58 @@ function queryBizwarnCaseCount(){
 			dataType: "json",
 			async : true,
 			success: function(data) {				
-				//黄灯任务
-				if(data.yeLight!=undefined && data.yeLight>0){
-					$("#yeLightCount").html(data.yeLight).parent('p').siblings("i").addClass('martop20');
-					$("#yeLightCount").parent('p').addClass('line').parents("li").addClass('light active').hover(function() {
+				
+				//无主资源
+				if(data.caseDistributeCount!=undefined && data.caseDistributeCount>0){
+					$('.light_info').prepend($("#caseDistributeCount").parents("li"));
+					$("#caseDistributeCount").html(data.caseDistributeCount).parent('p').siblings("i").addClass('martop20');
+					$("#caseDistributeCount").parent('p').addClass('line').parents("li").addClass('light active').hover(function() {
 				        $(this).css("background","#52cdec");
 				        $(this).find(".icon,.line,span").addClass('white');
 				    },function() {
 				        $(this).css("background","#edfcfd");
 				        $(this).find(".icon,.line,span").removeClass('white');
 				    });
-					$("#yeLightCount").parents("li").children().wrapAll(function() {
-						  return '<a href="'+ctx+'/workspace/ryLightList?color=1" target="_blank"></a>';
+					$("#caseDistributeCount").parents("li").children().wrapAll(function() {
+						  return '<a href="'+ctx+'/case/caseDistribute" target="_blank"></a>';
 					});
 				}
-				//红灯任务
-				if(data.redLight!=undefined && data.redLight>0){
-					$("#redLightCount").html(data.redLight).parent('p').siblings("i").addClass('martop20');
-					$("#redLightCount").parent('p').addClass('line').parents("li").addClass('light active').hover(function() {
+				
+				//无主任务
+				if(data.unLocatedTaskCount!=undefined && data.unLocatedTaskCount>0){
+					$('.light_info').prepend($("#unLocatedTaskCount").parents("li"));
+					$("#unLocatedTaskCount").html(data.unLocatedTaskCount).parent('p').siblings("i").addClass('martop20');
+					$("#unLocatedTaskCount").parent('p').addClass('line').parents("li").addClass('light active').hover(function() {
 				        $(this).css("background","#52cdec");
 				        $(this).find(".icon,.line,span").addClass('white');
 				    },function() {
 				        $(this).css("background","#edfcfd");
 				        $(this).find(".icon,.line,span").removeClass('white');
 				    });
-					$("#redLightCount").parents("li").children().wrapAll(function() {
-						  return '<a href="'+ctx+'/workspace/ryLightList?color=0" target="_blank"></a>';
+					$("#unLocatedTaskCount").parents("li").children().wrapAll(function() {
+						  return '<a href="'+ctx+'/unlocatedTasks" target="_blank"></a>';
 					});
 				}
+				
+				//无主案件
+				if(data.unLocatedCaseCount!=undefined && data.unLocatedCaseCount>0){
+					$('.light_info').prepend($("#unLocatedCaseCount").parents("li"));
+					$("#unLocatedCaseCount").html(data.unLocatedCaseCount).parent('p').siblings("i").addClass('martop20');
+					$("#unLocatedCaseCount").parent('p').addClass('line').parents("li").addClass('light active').hover(function() {
+				        $(this).css("background","#52cdec");
+				        $(this).find(".icon,.line,span").addClass('white');
+				    },function() {
+				        $(this).css("background","#edfcfd");
+				        $(this).find(".icon,.line,span").removeClass('white');
+				    });
+					$("#unLocatedCaseCount").parents("li").children().wrapAll(function() {
+						  return '<a href="'+ctx+'/case/unlocatedCase" target="_blank"></a>';
+					});
+				}
+				
 				//贷款流失预警
 				if(data.bizwarnCaseCount!=undefined && data.bizwarnCaseCount>0){
+					$('.light_info').prepend($("#bizwarnCaseCount").parents("li"));
 					$("#bizwarnCaseCount").html(data.bizwarnCaseCount).parent('p').siblings("i").addClass('martop20');
 					$("#bizwarnCaseCount").parent('p').addClass('line').parents("li").addClass('light active').hover(function() {
 				        $(this).css("background","#52cdec");
@@ -51,47 +73,35 @@ function queryBizwarnCaseCount(){
 					});
 				}
 				
+				//红灯任务
+				if(data.redLight!=undefined && data.redLight>0){
+					$('.light_info').prepend($("#redLightCount").parents("li"));
+					$("#redLightCount").html(data.redLight).parent('p').siblings("i").addClass('martop20');
+					$("#redLightCount").parent('p').addClass('line').parents("li").addClass('light active').hover(function() {
+				        $(this).css("background","#52cdec");
+				        $(this).find(".icon,.line,span").addClass('white');
+				    },function() {
+				        $(this).css("background","#edfcfd");
+				        $(this).find(".icon,.line,span").removeClass('white');
+				    });
+					$("#redLightCount").parents("li").children().wrapAll(function() {
+						  return '<a href="'+ctx+'/workspace/ryLightList?color=0" target="_blank"></a>';
+					});
+				}
 				
-				//无主案件
-				if(data.unLocatedCaseCount!=undefined && data.unLocatedCaseCount>0){
-					$("#unLocatedCaseCount").html(data.unLocatedCaseCount).parent('p').siblings("i").addClass('martop20');
-					$("#unLocatedCaseCount").parent('p').addClass('line').parents("li").addClass('light active').hover(function() {
+				//黄灯任务
+				if(data.yeLight!=undefined && data.yeLight>0){
+					$('.light_info').prepend($("#yeLightCount").parents("li"));
+					$("#yeLightCount").html(data.yeLight).parent('p').siblings("i").addClass('martop20');
+					$("#yeLightCount").parent('p').addClass('line').parents("li").addClass('light active').hover(function() {
 				        $(this).css("background","#52cdec");
 				        $(this).find(".icon,.line,span").addClass('white');
 				    },function() {
 				        $(this).css("background","#edfcfd");
 				        $(this).find(".icon,.line,span").removeClass('white');
 				    });
-					$("#unLocatedCaseCount").parents("li").children().wrapAll(function() {
-						  return '<a href="'+ctx+'/case/unlocatedCase" target="_blank"></a>';
-					});
-				}
-				//无主任务
-				if(data.unLocatedTaskCount!=undefined && data.unLocatedTaskCount>0){
-					$("#unLocatedTaskCount").html(data.unLocatedTaskCount).parent('p').siblings("i").addClass('martop20');
-					$("#unLocatedTaskCount").parent('p').addClass('line').parents("li").addClass('light active').hover(function() {
-				        $(this).css("background","#52cdec");
-				        $(this).find(".icon,.line,span").addClass('white');
-				    },function() {
-				        $(this).css("background","#edfcfd");
-				        $(this).find(".icon,.line,span").removeClass('white');
-				    });
-					$("#unLocatedTaskCount").parents("li").children().wrapAll(function() {
-						  return '<a href="'+ctx+'/unlocatedTasks" target="_blank"></a>';
-					});
-				}
-				//无主资源
-				if(data.caseDistributeCount!=undefined && data.caseDistributeCount>0){
-					$("#caseDistributeCount").html(data.caseDistributeCount).parent('p').siblings("i").addClass('martop20');
-					$("#caseDistributeCount").parent('p').addClass('line').parents("li").addClass('light active').hover(function() {
-				        $(this).css("background","#52cdec");
-				        $(this).find(".icon,.line,span").addClass('white');
-				    },function() {
-				        $(this).css("background","#edfcfd");
-				        $(this).find(".icon,.line,span").removeClass('white');
-				    });
-					$("#caseDistributeCount").parents("li").children().wrapAll(function() {
-						  return '<a href="'+ctx+'/case/caseDistribute" target="_blank"></a>';
+					$("#yeLightCount").parents("li").children().wrapAll(function() {
+						  return '<a href="'+ctx+'/workspace/ryLightList?color=1" target="_blank"></a>';
 					});
 				}
 				
