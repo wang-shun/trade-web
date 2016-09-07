a141d58dc1dbb6af929ef2bfeeb4fcfc4a6fa126
yinjf2 <yinjf2@centaline.com.cn>
2016-09-07 17:41:35 +0800

    添加产品类别和产品接口

---
+++ trade-core/src/main/java/com/centaline/trans/product/entity/Product.java
@@ -0,0 +1,115 @@
+package com.centaline.trans.product.entity;
+
+import java.util.Date;
+
+public class Product {
+	private Long pkid;
+
+	private String prdcCode;
+
+	private String prodCode;
+
+	private String vendor;
+
+	private Integer recRate;
+
+	private Date createTime;
+
+	private String createBy;
+
+	private Date updateTime;
+
+	private String updateBy;
+
+	private String sort; // 排序字段
+
+	private String status; // 状态(0:启用,1:禁用,2:删除)
+
+	public Long getPkid() {
+		return pkid;
+	}
+
+	public void setPkid(Long pkid) {
+		this.pkid = pkid;
+	}
+
+	public String getPrdcCode() {
+		return prdcCode;
+	}
+
+	public void setPrdcCode(String prdcCode) {
+		this.prdcCode = prdcCode == null ? null : prdcCode.trim();
+	}
+
+	public String getProdCode() {
+		return prodCode;
+	}
+
+	public void setProdCode(String prodCode) {
+		this.prodCode = prodCode == null ? null : prodCode.trim();
+	}
+
+	public String getVendor() {
+		return vendor;
+	}
+
+	public void setVendor(String vendor) {
+		this.vendor = vendor == null ? null : vendor.trim();
+	}
+
+	public Integer getRecRate() {
+		return recRate;
+	}
+
+	public void setRecRate(Integer recRate) {
+		this.recRate = recRate;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public String getCreateBy() {
+		return createBy;
+	}
+
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy == null ? null : createBy.trim();
+	}
+
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+
+	public String getUpdateBy() {
+		return updateBy;
+	}
+
+	public void setUpdateBy(String updateBy) {
+		this.updateBy = updateBy == null ? null : updateBy.trim();
+	}
+
+	public String getSort() {
+		return sort;
+	}
+
+	public void setSort(String sort) {
+		this.sort = sort == null ? null : sort.trim();
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status == null ? null : status.trim();
+	}
+}
