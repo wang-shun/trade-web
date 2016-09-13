9ba6fd2c026ec17dc98d323554ba159a4c840f69
gongjieduo <gongjd@centaline.com.cn>
2016-09-13 10:59:46 +0800

    金额计算
    Change-Id: I91d4a68e5ce2523beaf5eeee0b47cfc06d38d1ad
    Signed-off-by: gongjieduo <gongjd@centaline.com.cn>

--- trade-web/src/main/webapp/static_res/trans/js/spv/spvDetails.js
+++ trade-web/src/main/webapp/static_res/trans/js/spv/spvDetails.js
@@ -1071,7 +1071,7 @@ function accDiv(arg1,arg2){
     with(Math){
         r1=Number(arg1.toString().replace(".",""));
         r2=Number(arg2.toString().replace(".",""));
-        return (r1/r2)*pow(10,t2-t1);
+        return ((r1/r2)*pow(10,t2-t1)).toFixed(2);
     }
 }
 //给Number类型增加一个div方法，调用起来更加方便。
@@ -1087,7 +1087,7 @@ function accMul(arg1,arg2)
     var m=0,s1=arg1.toString(),s2=arg2.toString();
     try{m+=s1.split(".")[1].length}catch(e){}
     try{m+=s2.split(".")[1].length}catch(e){}
-    return Number(s1.replace(".",""))*Number(s2.replace(".",""))/Math.pow(10,m);
+    return (Number(s1.replace(".",""))*Number(s2.replace(".",""))/Math.pow(10,m)).toFixed(2);
 }
 //给Number类型增加一个mul方法，调用起来更加方便。
 Number.prototype.mul = function (arg){
@@ -1102,7 +1102,7 @@ function accAdd(arg1,arg2){
     try{r1=arg1.toString().split(".")[1].length}catch(e){r1=0}
     try{r2=arg2.toString().split(".")[1].length}catch(e){r2=0}
     m=Math.pow(10,Math.max(r1,r2));
-    return (arg1*m+arg2*m)/m;
+    return ((arg1*m+arg2*m)/m).toFixed(2);
 }
 //给Number类型增加一个add方法，调用起来更加方便。
 Number.prototype.add = function (arg){
