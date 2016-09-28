59c84e877ebfef5122ea5e982bee841b78327102
zhoujp7 <zhoujp7@SHCYF62T.centaline.com.cn>
2016-09-28 17:39:04 +0800

    签约室

--- trade-web/src/main/webapp/static/transjs/signing/sigingManager.js
+++ trade-web/src/main/webapp/static/transjs/signing/sigingManager.js
@@ -252,6 +252,7 @@ function updateSignRoom(centerId,roomNo,numbeOfAccommodatePeople,roomType,remark
 	$("#pkid").val(pkid);
 	$("select[name='rmSignRoom.centerId']").val(centerId);
   	$("input[name='rmSignRoom.roomNo']").val(roomNo);
+  	$("input[name='rmSignRoom.roomNo']").prop("readonly","readonly");
   	$("input[name='rmSignRoom.numbeOfAccommodatePeople']").val(numbeOfAccommodatePeople);
   	$("input[name='rmSignRoom.roomType']").each(function(){
        if($(this).prop("value")==roomType){
