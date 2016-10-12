3437179785975eb32eb415c507f48b9a386ee1f7
zhuody <zhuody@centaline.com.cn>
2016-10-12 17:05:34 +0800

    入账 凭证编号验证

--- trade-web/src/main/webapp/static_res/trans/js/spv/spvReDetails.js
+++ trade-web/src/main/webapp/static_res/trans/js/spv/spvReDetails.js
@@ -23,7 +23,7 @@ function getTR(thisIndex){
 	$str+='		<input class="boderbbt" style="border:none;width: 50px;" type="text" placeholder="金额" onKeypress="if (!(event.keyCode > 45 && event.keyCode < 58 &&event.keyCode !=47 ) ) event.returnValue = false;" name="items['+thisIndex+'].payerAmount">万元                                                                        ';
 	$str+='	</td>                                                                                                                                                                                      ';
 	$str+='	<td>                                                                                                                                                                                       ';
-	$str+='		<input class="table_input boderbbt" type="text" placeholder="请输入编号" onKeypress="if ((event.keyCode > 32 && event.keyCode < 48) || (event.keyCode > 57 && event.keyCode < 65) || (event.keyCode > 90 && event.keyCode < 97)) event.returnValue = false;" name="items['+thisIndex+'].receiptNo">                                                                                             ';
+	$str+='		<input class="table_input boderbbt forvalue" type="text" placeholder="请输入编号" onKeypress="if ((event.keyCode > 32 && event.keyCode < 48) || (event.keyCode > 57 && event.keyCode < 65) || (event.keyCode > 90 && event.keyCode < 97)) event.returnValue = false;" name="items['+thisIndex+'].receiptNo">                                                                                             ';
 	$str+='	</td>                                                                                                                                                                                      ';
 	$str+='	<td>                                                                                                                                                                                       ';
 	$str+='		<select name="items['+thisIndex+'].voucherNo" class="table-select boderbbt" onChange="this.value">                                                                                                                                ';
@@ -73,11 +73,31 @@ function getDel(k){
     cleanPkid();
 }
 
+function checkReceiptNo(){
+	var theSameFlag = true;
+	var receiptNoArray = new Array();
+	receiptNoArray = $(".forvalue");
+	for(var i=0; i<receiptNoArray.length; i++){		
+		if(receiptNoArray[i].value==receiptNoArray[i+1].value){
+			theSameFlag=false;
+			alert("贷记凭证编号不能重复！");
+			if(theSameFlag==false){
+				break;
+			}
+		}
+
+	}
+	return theSameFlag;
+}
 //提交
 function sumbitRe(){
 	if(!confirm("是否确定提交申请，开启流程！")){
 	  return false;
     }
+	
+	if(!checkReceiptNo()){
+		return;
+	}	
 	//提交页面的参数
 	var data = $("#teacForm").serialize();
 	//console.log(data);
