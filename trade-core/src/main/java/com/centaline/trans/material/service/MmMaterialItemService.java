e14ad81c5f833b4edd5f112c2ac419e57b8aa440
zhuody <zhuody@centaline.com.cn>
2016-10-19 16:04:49 +0800

    风控、物品数据同步接口

---
+++ trade-core/src/main/java/com/centaline/trans/material/service/MmMaterialItemService.java
@@ -0,0 +1,14 @@
+package com.centaline.trans.material.service;
+
+import java.util.List;
+
+import com.centaline.trans.material.entity.MmMaterialItem;
+
+public interface MmMaterialItemService {
+	
+    List<MmMaterialItem> queryMmMaterialItemList();
+    
+    void  insertMaterialInfoFromSpv(MmMaterialItem mmMaterialItem);
+    
+    int  updateMaterialInfoByCaseCode(String itemCode);
+}
