852d4995ad191207bd1d599f4c806bf99f4b0ea6
zhoujp7 <zhoujp7@SHCYF62T.centaline.com.cn>
2016-10-20 09:58:43 +0800

    交易计划变更列表

--- trade-core/src/main/java/com/centaline/trans/cases/repository/ReturnVisitRegistrationMapper.java
+++ trade-core/src/main/java/com/centaline/trans/cases/repository/ReturnVisitRegistrationMapper.java
@@ -7,6 +7,16 @@ import com.centaline.trans.common.MyBatisRepository;
 
 @MyBatisRepository
 public interface ReturnVisitRegistrationMapper {
-	
+	/**
+	 * 跟进交易变更历史表id查询回访跟进信息
+	 * @param historyId
+	 * @return
+	 */
 	List<CaseReturnVisitRegistrationVO> queryReturnVisitRegistrations(Long historyId);
+	/**
+	 * 新增回访跟进信息
+	 * @param caseReturnVisitRegistrationVO
+	 * @return
+	 */
+	int insertReturnVisitRegistration(CaseReturnVisitRegistrationVO caseReturnVisitRegistrationVO);
 }
