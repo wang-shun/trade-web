4e02d845f293dee90e5bfbeb90a48f4683a3d65c
ouyq <1737718880@qq.com>
2016-10-25 17:25:33 +0800

    案件导入导出页面

---
+++ trade-web/src/main/java/com/centaline/trans/sys/web/CaseModifyController.java
@@ -0,0 +1,57 @@
+package com.centaline.trans.sys.web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.aist.common.web.validate.AjaxResponse;
+import com.centaline.trans.cases.service.ToCaseInfoService;
+
+@Controller
+@RequestMapping(value = "/caseModify")
+public class CaseModifyController {
+
+	@Autowired
+	private ToCaseInfoService toCaseInfoService;
+
+	// 列表页面
+	@RequestMapping("toCaseModify")
+	public String spvList() {
+		return "sys/caseModify";
+	}
+
+	/**
+	 * 修改TagergetCode
+	 * 
+	 * @param flag
+	 * @return
+	 */
+	@RequestMapping(value = "updateTagetCode.json")
+	@ResponseBody
+	public AjaxResponse updateTagetCode(@RequestParam(required = true) String ctmCode,
+			@RequestParam(required = true) String targetCode) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("ctmCode", ctmCode);
+		map.put("targetCode", targetCode);
+		Integer result = toCaseInfoService.updateByTargetCode(map);
+		return new AjaxResponse(result == 1);
+	}
+	
+	/**
+	 * 导入ctm案件
+	 * 
+	 * @param flag
+	 * @return
+	 */
+	@RequestMapping(value = "exportCTMCase.json")
+	@ResponseBody
+	public AjaxResponse exportCTMCase(@RequestParam(required = true) String ctmCode){
+		Integer result = toCaseInfoService.exportCTMCase(ctmCode);
+		return new AjaxResponse(result == 1);
+	}
+}
