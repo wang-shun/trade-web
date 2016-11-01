132ff9c67faefe7d3544445f3e9cbb1a6ff05593
zhuody <zhuody@centaline.com.cn>
2016-11-01 16:12:40 +0800

    置灰、物品类型保存

--- trade-web/src/main/java/com/centaline/trans/material/web/MaterialManagementController.java
+++ trade-web/src/main/java/com/centaline/trans/material/web/MaterialManagementController.java
@@ -309,19 +309,7 @@ public class MaterialManagementController {
 			//插入操作获取pkid
 			for(int i=0; i<materialList.size() ;i++){
 				//更新主表状态
-				MmMaterialItem mmMaterialItem = materialList.get(i);
-				
-				if(mmMaterialItem.getItemCategory().equals("身份证")){
-					mmMaterialItem.setItemCategory("carded");
-				}else if(mmMaterialItem.getItemCategory().equals("抵押合同")){
-					mmMaterialItem.setItemCategory("mortgageContract");
-				}else if(mmMaterialItem.getItemCategory().equals("银行卡")){
-					mmMaterialItem.setItemCategory("bankCard");
-				}else if(mmMaterialItem.getItemCategory().equals("产权证")){
-					mmMaterialItem.setItemCategory("propertyCard");
-				}else if(mmMaterialItem.getItemCategory().equals("他证")){
-					mmMaterialItem.setItemCategory("otherCard");
-				}					
+				MmMaterialItem mmMaterialItem = materialList.get(i);				
 				mmMaterialItem.setItemAddrCode(itemAddrCode);
 				mmMaterialItem.setItemManager(userId);
 				mmMaterialItem.setItemStatus(MaterialStatusEnum.INSTOCK.getCode());
