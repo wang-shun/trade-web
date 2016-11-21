0105cc7a3edd7de4523c418f2ba4ea1d8acc3c88
jimmy <504562194@qq.com>
2016-11-21 16:21:49 +0800

    将KPI包移到AWARD包中

---
+++ trade-core/src/main/java/com/centaline/trans/award/repository/ToPropertyResearchAttachMapper.java
@@ -0,0 +1,17 @@
+package com.centaline.trans.award.repository;
+
+import com.centaline.trans.common.repository.ToPropertyResearchAttach;
+
+public interface ToPropertyResearchAttachMapper {
+    int deleteByPrimaryKey(Long pkid);
+
+    int insert(ToPropertyResearchAttach record);
+
+    int insertSelective(ToPropertyResearchAttach record);
+
+    ToPropertyResearchAttach selectByPrimaryKey(Long pkid);
+
+    int updateByPrimaryKeySelective(ToPropertyResearchAttach record);
+
+    int updateByPrimaryKey(ToPropertyResearchAttach record);
+}
