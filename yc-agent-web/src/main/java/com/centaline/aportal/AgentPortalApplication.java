3cdb799ec6280786a98e50b687f9c6a2b2e76aa3
jimmy <504562194@qq.com>
2016-12-01 11:47:05 +0800

    spring boot

--- yc-agent-web/src/main/java/com/centaline/aportal/AgentPortalApplication.java
+++ yc-agent-web/src/main/java/com/centaline/aportal/AgentPortalApplication.java
@@ -1,12 +1,55 @@
 package com.centaline.aportal;
 
+import java.util.Arrays;
+
+import org.jasig.cas.client.session.SingleSignOutFilter;
+import org.jasig.cas.client.session.SingleSignOutHttpSessionListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.filter.DelegatingFilterProxy;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.aist.uam", "com.centaline.aportal" }, excludeFilters = {
+		@ComponentScan.Filter(type=FilterType.REGEX, pattern = "com.aist.uam.basedata.Repository.impl.*") })
 public class AgentPortalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AgentPortalApplication.class, args);
+		SpringApplication application = new SpringApplication(AgentPortalApplication.class);
+		application.run(args);
+	}
+
+	@Bean
+	public FilterRegistrationBean singleSignOutFilterRegistrationBean() {
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		filterRegistrationBean.setFilter(new SingleSignOutFilter());
+		filterRegistrationBean.setName("CAS Single Sign Out Filter");
+		filterRegistrationBean.setUrlPatterns(Arrays.asList("/*"));
+		return filterRegistrationBean;
+	}
+
+	@Bean
+	public FilterRegistrationBean shiroFilterRegistrationBean() {
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		DelegatingFilterProxy dfp = new DelegatingFilterProxy();
+		dfp.setBeanName("shiroFilter");
+		dfp.setTargetFilterLifecycle(true);
+		filterRegistrationBean.setFilter(dfp);
+		filterRegistrationBean.setUrlPatterns(Arrays.asList("/*"));
+		filterRegistrationBean.setName("shiroFilter");
+		return filterRegistrationBean;
+	}
+
+	// SingleSignOutHttpSessionListener
+	@Bean
+	public ServletListenerRegistrationBean<SingleSignOutHttpSessionListener> singleSignoutRegistrationBean() {
+		ServletListenerRegistrationBean<SingleSignOutHttpSessionListener> registrationBean = new ServletListenerRegistrationBean<>();
+		registrationBean.setListener(new SingleSignOutHttpSessionListener());
+		return registrationBean;
 	}
 }
--- yc-agent-web/src/main/java/com/centaline/aportal/demo/web/DemoController.java
+++ yc-agent-web/src/main/java/com/centaline/aportal/demo/web/DemoController.java
@@ -3,6 +3,9 @@
  */
 package com.centaline.aportal.demo.web;
 
+import com.aist.uam.auth.remote.UamSessionService;
+import com.aist.uam.conf.ShiroConfig;
+import com.aist.uam.permission.remote.UamPermissionService;
 import com.centaline.aportal.demo.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,13 +16,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Created by linjiarong on 2016/10/12.
  */
 @Controller
-@RequestMapping("/demo")
 public class DemoController {
+	@Autowired
+	private UamSessionService uamsessionService;
     @Autowired
     private DemoService demoService;
+    @Autowired
+    private UamPermissionService uamPermissionServiceClient;
 
-    @RequestMapping("/")
-    public @ResponseBody String demoTest(){
+    @RequestMapping("/test")
+    public @ResponseBody String demoTest(String code){
+    	uamPermissionServiceClient.findMenuByCode(code);
+    	System.out.println(uamsessionService.getSessionUser());
         return demoService.helloworld();
     }
 }
