8bdd1519e95e7ce9c43acbf2ad127632375316da
yinjf2 <yinjf2@centaline.com.cn>
2016-12-07 15:02:45 +0800

    流程重启页面改版

--- trade-web/src/main/java/com/centaline/trans/cases/web/ServiceRestartController.java
+++ trade-web/src/main/java/com/centaline/trans/cases/web/ServiceRestartController.java
@@ -17,7 +17,7 @@ import com.centaline.trans.cases.service.ToCaseService;
 import com.centaline.trans.cases.vo.CaseBaseVO;
 import com.centaline.trans.cases.vo.ServiceRestartVo;
 import com.centaline.trans.engine.vo.StartProcessInstanceVo;
-
+import com.centaline.trans.utils.UiImproveUtil;
 
 @Controller
 @RequestMapping(value = "/service")
@@ -28,54 +28,61 @@ public class ServiceRestartController {
 	private UamSessionService uamSessionService;
 	@Autowired
 	private ToCaseService toCaseService;
+
 	/**
 	 * 
 	 * @return
 	 */
 	@RequestMapping(value = "/restart")
 	@ResponseBody
-	public AjaxResponse<StartProcessInstanceVo> restart(Model model, ServiceRestartVo vo) {
-		SessionUser u= uamSessionService.getSessionUser();
+	public AjaxResponse<StartProcessInstanceVo> restart(Model model,
+			ServiceRestartVo vo) {
+		SessionUser u = uamSessionService.getSessionUser();
 		vo.setUserId(u.getId());
 		vo.setUserName(u.getUsername());
 		vo.setOrgId(u.getServiceDepId());
 		// 删除相关
-		StartProcessInstanceVo piv = serviceRestart.restartAndDeleteSubProcess(vo);
+		StartProcessInstanceVo piv = serviceRestart
+				.restartAndDeleteSubProcess(vo);
 		AjaxResponse<StartProcessInstanceVo> resp = new AjaxResponse<>();
 		resp.setContent(piv);
 		return resp;
 	}
+
 	@RequestMapping("apply/process")
-	public String toApplyProcess(HttpServletRequest request, HttpServletResponse response,
-			String caseCode, String source, String taskitem, String processInstanceId) {
-		SessionUser user= uamSessionService.getSessionUser();
+	public String toApplyProcess(HttpServletRequest request,
+			HttpServletResponse response, String caseCode, String source,
+			String taskitem, String processInstanceId) {
+		SessionUser user = uamSessionService.getSessionUser();
 		CaseBaseVO caseBaseVO = toCaseService.getCaseBaseVO(caseCode);
 		request.setAttribute("source", source);
 		request.setAttribute("caseBaseVO", caseBaseVO);
-		
+
 		request.setAttribute("approveType", "7");
 		request.setAttribute("operator", user != null ? user.getId() : "");
-		return "task/taskserviceRestartApply";
+		return "task" + UiImproveUtil.getPageType(request)
+				+ "/taskserviceRestartApply";
 	}
+
 	@RequestMapping("approve/process")
-	public String toApproveProcess(HttpServletRequest request, HttpServletResponse response,
-			String caseCode, String source, String taskitem, String processInstanceId) {
-		SessionUser user= uamSessionService.getSessionUser();
+	public String toApproveProcess(HttpServletRequest request,
+			HttpServletResponse response, String caseCode, String source,
+			String taskitem, String processInstanceId) {
+		SessionUser user = uamSessionService.getSessionUser();
 		CaseBaseVO caseBaseVO = toCaseService.getCaseBaseVO(caseCode);
-		//税费卡
+		// 税费卡
 		int cou = toCaseService.findToLoanAgentByCaseCode(caseCode);
-		if ( cou >0) {
+		if (cou > 0) {
 			caseBaseVO.setLoanType("30004005");
 		}
 		request.setAttribute("source", source);
 		request.setAttribute("caseBaseVO", caseBaseVO);
-		
+
 		request.setAttribute("approveType", "7");
 		request.setAttribute("operator", user != null ? user.getId() : "");
 		return "task/taskserviceRestartApprove";
 	}
-	
-	
+
 	/**
 	 * 
 	 * @return
@@ -83,14 +90,14 @@ public class ServiceRestartController {
 	@RequestMapping(value = "/apply")
 	@ResponseBody
 	public AjaxResponse apply(Model model, ServiceRestartVo vo) {
-		SessionUser u= uamSessionService.getSessionUser();
+		SessionUser u = uamSessionService.getSessionUser();
 		vo.setUserId(u.getId());
 		vo.setUserName(u.getUsername());
 		boolean piv = serviceRestart.apply(vo);
 		AjaxResponse resp = new AjaxResponse(piv);
 		return resp;
 	}
-	
+
 	/**
 	 * 
 	 * @return
@@ -98,7 +105,7 @@ public class ServiceRestartController {
 	@RequestMapping(value = "/approve")
 	@ResponseBody
 	public AjaxResponse approve(Model model, ServiceRestartVo vo) {
-		SessionUser u= uamSessionService.getSessionUser();
+		SessionUser u = uamSessionService.getSessionUser();
 		vo.setUserId(u.getId());
 		vo.setUserName(u.getUsername());
 		boolean piv = serviceRestart.approve(vo);
