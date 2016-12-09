42fa9e7d3d68278af40350c3f6ebb06414a4c72a
hujunnan <hujunnan@aisthink.com>
2016-12-09 14:10:45 +0800

    modify to war

--- yc-par-portal/src/main/java/com/centaline/parportal/ParPortalApplication.java
+++ yc-par-portal/src/main/java/com/centaline/parportal/ParPortalApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = { "com.centaline.parportal", "com.aist" })
 @SpringBootApplication
 public class ParPortalApplication extends SpringBootServletInitializer {
 
