c43a8e977570224640ca01067451b79dbc0edba9
hujunnan <hujunnan@aisthink.com>
2016-12-09 11:07:59 +0800

    modify build to war

--- yc-par-portal/src/main/java/com/centaline/parportal/ParPortalApplication.java
+++ yc-par-portal/src/main/java/com/centaline/parportal/ParPortalApplication.java
@@ -1,28 +1,22 @@
 package com.centaline.parportal;
 
-import java.util.Arrays;
-
-import org.jasig.cas.client.session.SingleSignOutFilter;
-import org.jasig.cas.client.session.SingleSignOutHttpSessionListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
-import org.springframework.context.annotation.Bean;
-
-import org.springframework.web.filter.DelegatingFilterProxy;
-
-
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ParPortalApplication {
+public class ParPortalApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication application = new SpringApplication(ParPortalApplication.class);
-		application.run(args);
-		
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ParPortalApplication.class);
+    }
 
-	
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(ParPortalApplication.class);
+        application.run(args);
+
+    }
+
 }
