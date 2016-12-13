a181b9a3ab6d8e4553777351aa1bd3f075f9b0b6
yinjf2 <yinjf2@centaline.com.cn>
2016-12-13 14:32:53 +0800

    过户驳回率统计报表优化

--- trade-web/src/main/java/com/centaline/trans/report/web/CaseRejectionRateController.java
+++ trade-web/src/main/java/com/centaline/trans/report/web/CaseRejectionRateController.java
@@ -1,11 +1,14 @@
 package com.centaline.trans.report.web;
 
 import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.centaline.trans.utils.UiImproveUtil;
 
 /**
  * 
@@ -26,9 +29,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CaseRejectionRateController {
 	@RequestMapping(value = "caseLossRateReasonList")
 	@RequiresPermissions("TRADE.REPORT.CASELOSS")
-	public String caseLossRateReasonList(Model model, ServletRequest request) {
-		return "report/case_loss_rate_reason_list";
+	public String caseLossRateReasonList(Model model, HttpServletRequest request) {
+		return "report" + UiImproveUtil.getPageType(request)
+				+ "/case_loss_rate_reason_list";
 	}
+
 	@RequestMapping(value = "caseLossRateList")
 	@RequiresPermissions("TRADE.REPORT.CASELOSSRATE")
 	public String caseLossrateList(Model model, ServletRequest request) {
