079fac75e238d56478bf422e851351d3c68b670a
linjiarong <linjr@aisthink.com>
2016-12-14 18:51:44 +0800

    fix QuickQuery get  HisUser

---
+++ trade-core/src/main/java/com/centaline/trans/common/service/impl/QuickQueryUserByUsernameCustomDictServiceImpl.java
@@ -0,0 +1,65 @@
+package com.centaline.trans.common.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+
+import com.aist.common.quickQuery.service.CustomDictService;
+import com.aist.uam.userorg.remote.UamUserOrgService;
+import com.aist.uam.userorg.remote.vo.User;
+
+public class QuickQueryUserByUsernameCustomDictServiceImpl implements CustomDictService{
+	
+	@Autowired
+	private UamUserOrgService uamUserOrgService;
+
+	private String prop;
+	
+	@Override
+	@Cacheable(value="QuickQueryUserByUsernameCustomDictServiceImpl",key="#root.target.getProp()+'/'+#key")
+	public String getValue(String key) {
+		User u = uamUserOrgService.getHisUserByUsername(key);
+		String value = null;
+		try {
+			value = BeanUtils.getProperty(u, prop);
+		} catch (Exception e) {
+			return null;
+		} 
+		return value;
+	}
+	
+	@Override
+	@Cacheable(value="QuickQueryUserByUsernameCustomDictServiceImpl",key="#root.target.getProp()+'/'+#keys")
+	public List<Map<String, Object>> findDicts(List<Map<String, Object>> keys) {
+		for(Map<String, Object> keyer:keys){
+			String val = "";
+			try {
+				Object key = keyer.values().iterator().next();
+				if(key!=null){
+					User u = uamUserOrgService.getHisUserByUsername(key.toString());
+					val = BeanUtils.getProperty(u, prop);
+				}
+			} catch (Exception e) {
+			} 
+			keyer.put("val", val);
+		}
+		
+		return keys;
+	}
+	
+	@Override
+	public Boolean getIsBatch() {
+		return true;
+	}
+	
+	public String getProp() {
+		return prop;
+	}
+
+	public void setProp(String prop) {
+		this.prop = prop;
+	}
+}
