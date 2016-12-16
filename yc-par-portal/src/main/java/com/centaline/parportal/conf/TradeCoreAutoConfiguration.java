3da05686a8c18aae90e2bbf4875cc1c6996b12af
hujunnan <hujunnan@aisthink.com>
2016-12-16 16:29:43 +0800

    par-portal mybatis

--- yc-par-portal/src/main/java/com/centaline/parportal/conf/TradeCoreAutoConfiguration.java
+++ yc-par-portal/src/main/java/com/centaline/parportal/conf/TradeCoreAutoConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration()
-@ComponentScan(basePackages = { "com.centaline.sctrans" })
+@ComponentScan(basePackages = { "com.centaline.trans.comment" })
 //@ImportResource(value = { "classpath*:com/aist/common/**/META-INF/beans.xml" })
 public class TradeCoreAutoConfiguration {
 
