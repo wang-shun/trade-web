234ee7455028cd3e746417f1b66200f14b4acd5d
linjiarong <linjr@aisthink.com>
2016-12-28 14:33:58 +0800

    fix docker make

--- trade-web/Dockermake.sh
+++ trade-web/Dockermake.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 pomver=`mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
-version=$pomver-${BUILD_NUMBER}
+version=$pomver
 
-docker build -t docker.aist.io/trade-web:${BUILD_NUMBER} .
+docker build -t docker.aist.io/trade-web:$version .
 
-docker tag docker.aist.io/trade-web:${BUILD_NUMBER} docker.aist.io/trade-web:$version
-docker tag docker.aist.io/trade-web:${BUILD_NUMBER} docker.aist.io/trade-web:latest
+docker tag docker.aist.io/trade-web:$version docker.aist.io/trade-web:latest
 
-docker push docker.aist.io/trade-web:${BUILD_NUMBER}
 docker push docker.aist.io/trade-web:$version
 docker push docker.aist.io/trade-web:latest
 
-docker rmi docker.aist.io/trade-web:${BUILD_NUMBER}
 docker rmi docker.aist.io/trade-web:$version
 docker rmi docker.aist.io/trade-web:latest
