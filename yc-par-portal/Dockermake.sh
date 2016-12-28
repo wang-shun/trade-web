5cdd656b75c3e803789ea5e1569d3b496561f69a
linjiarong <linjr@aisthink.com>
2016-12-28 14:20:17 +0800

    fix docker make

--- yc-par-portal/Dockermake.sh
+++ yc-par-portal/Dockermake.sh
@@ -2,15 +2,12 @@
 pomver=`mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
 version=$pomver-${BUILD_NUMBER}
 
-docker build -t docker.aist.io/yc-par-web:${BUILD_NUMBER} .
+docker build -t docker.aist.io/yc-par-web:$version .
 
-docker tag docker.aist.io/yc-par-web:${BUILD_NUMBER} docker.aist.io/yc-par-web:$version
-docker tag docker.aist.io/yc-par-web:${BUILD_NUMBER} docker.aist.io/yc-par-web:latest
+docker tag docker.aist.io/yc-par-web:$version docker.aist.io/yc-par-web:latest
 
-docker push docker.aist.io/yc-par-web:${BUILD_NUMBER}
 docker push docker.aist.io/yc-par-web:$version
 docker push docker.aist.io/yc-par-web:latest
 
-docker rmi docker.aist.io/yc-par-web:${BUILD_NUMBER}
 docker rmi docker.aist.io/yc-par-web:$version
 docker rmi docker.aist.io/yc-par-web:latest
