048a94aaf529b7040499a9d04c76b272bbedb6c0
liaoqh2 <liaoqh2@centaline.com.cn>
2017-01-10 10:07:03 +0800

    删除sctrans.ACT_RU_EVENT_SUBSCR表增删改方法，流程引擎表数据只能通过远程接口进行操作。

--- trade-core/src/main/java/com/centaline/trans/task/repository/ActRuEventSubScrMapper.java
+++ trade-core/src/main/java/com/centaline/trans/task/repository/ActRuEventSubScrMapper.java
@@ -6,17 +6,8 @@ import com.centaline.trans.common.MyBatisRepository;
 import com.centaline.trans.task.entity.ActRuEventSubScr;
 @MyBatisRepository
 public interface ActRuEventSubScrMapper {
-    int deleteByPrimaryKey(String id);
-
-    int insert(ActRuEventSubScr record);
-
-    int insertSelective(ActRuEventSubScr record);
-
+	
     ActRuEventSubScr selectByPrimaryKey(String id);
-
-    int updateByPrimaryKeySelective(ActRuEventSubScr record);
-
-    int updateByPrimaryKey(ActRuEventSubScr record);
-    
+   
     List<ActRuEventSubScr>listBySelective(ActRuEventSubScr record);
 }
