4deb8035b41d913b6ce54987e2b6c5a7de9345e8
linjiarong <linjr@aisthink.com>
2017-01-20 01:55:53 +0800

    add sudo

--- trade-web/Dockermake.sh
+++ trade-web/Dockermake.sh
@@ -2,6 +2,6 @@
 pomver=`mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
 version=$pomver
 
-docker build -t docker.aist.io/trade-web:$version .
-docker push docker.aist.io/trade-web:$version
-docker rmi docker.aist.io/trade-web:$version
+sudo docker build -t docker.aist.io/trade-web:$version .
+sudo docker push docker.aist.io/trade-web:$version
+sudo docker rmi docker.aist.io/trade-web:$version
