78e2e4bdc14582b31d863bdd31195a0695d7d2b1
[caoyuan7] <caoyuan7@centaline.com.cn>
2017-01-22 18:03:13 +0800

    基础信息表存储过程修改和表结构修改

--- trade-web/src/main/resources/sql/更新报表的基础表和历史表字段.sql
+++ trade-web/src/main/resources/sql/更新报表的基础表和历史表字段.sql
@@ -3,5 +3,11 @@
 ALTER TABLE [sctrans].[T_RPT_CASE_BASE_INFO] ADD  MORTAGE_SIGN_DATE datetime;
 ALTER TABLE [sctrans].[T_RPT_HISTORY_CASE_BASE_INFO] ADD  MORTAGE_SIGN_DATE datetime;**/
 
-ALTER TABLE [sctrans].[T_RPT_CASE_BASE_INFO] ADD  SIGN_CON_PRICE datetime;
-ALTER TABLE [sctrans].[T_RPT_HISTORY_CASE_BASE_INFO] ADD  SIGN_CON_PRICE datetime;
+/*已执行过
+ALTER TABLE [sctrans].[T_RPT_CASE_BASE_INFO] alter column  SIGN_CON_PRICE [decimal](18, 2);
+ALTER TABLE [sctrans].[T_RPT_HISTORY_CASE_BASE_INFO] alter column  SIGN_CON_PRICE [decimal](18, 2);
+*/
+
+
+ALTER TABLE [sctrans].[T_RPT_CASE_BASE_INFO] alter column  SIGN_CON_PRICE [decimal](18, 2);
+ALTER TABLE [sctrans].[T_RPT_HISTORY_CASE_BASE_INFO] alter column  SIGN_CON_PRICE [decimal](18, 2);
