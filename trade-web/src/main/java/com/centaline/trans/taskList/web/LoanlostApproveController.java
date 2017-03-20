c3bd22eae7898ca84e794fafcf88d5a2ab6bcd58
hejf10 <hejf10@centaline.com.cn>
2017-03-20 17:14:58 +0800

    自办贷款

--- trade-web/src/main/java/com/centaline/trans/taskList/web/LoanlostApproveController.java
+++ trade-web/src/main/java/com/centaline/trans/taskList/web/LoanlostApproveController.java
@@ -312,32 +312,10 @@ public class LoanlostApproveController {
 		
 		/** 服务编码[srv_code]和案件编号[case_code]到服务表[T_TG_SERV_ITEM_AND_PROCESSOR]中去查询交易顾问id[processor_id] 30004010029交易过户（除签约外）)**/
 		String ts = toCaseService.selectServItem(processInstanceVO.getCaseCode(),"3000401002");
-		/*RestVariable rv = new RestVariable();
-		rv.setName("loanHandler");
-		rv.setValue(ts.getSrvName());
-		variables.add(rv);*/
-		
-	/*	RestVariable rvfk = new RestVariable();
-		
-		rvfk.setName("loanHandler");
-		rvfk.setValue("puyp,suad1,gufy03");
-		
-		List<RestVariable> vs = new ArrayList<RestVariable>();
-		vs.add(rv);
-		rv.setName("loanHandler");
-		rv.setValue("xuqq");
-		vs.add(rv);
-		RestVariable rvf = new RestVariable();
-		rvf.setName("loanHandler");
-		rvf.setValue("xuqq,hujh");
-		
-		String ts = "xuqq,hujh";*/
 		List<String> membersList = null;
-		List<RestVariable> a = new ArrayList<RestVariable>();
 		if(ts != null && ts.length() > 0){
 			membersList = Arrays.asList(ts.split(","));
 		}
-		
 		variables.add(new RestVariable("loanHandlers",membersList)); 
 		
 		
