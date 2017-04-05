d291b4cde82e49f235bf3091bc01d8393759dfe0
yinjf2 <yinjf2@centaline.com.cn>
2017-04-05 18:16:45 +0800

    修改按揭列表出错的问题

--- trade-mobile/src/main/java/com/centaline/parportal/mobile/track/web/TrackController.java
+++ trade-mobile/src/main/java/com/centaline/parportal/mobile/track/web/TrackController.java
@@ -4,8 +4,6 @@
  */
 package com.centaline.parportal.mobile.track.web;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aist.common.exception.BusinessException;
-import com.centaline.parportal.mobile.mortgage.web.MortgageListController;
 import com.centaline.parportal.mobile.track.vo.CommentVo;
 import com.centaline.trans.comment.entity.ToCaseComment;
 import com.centaline.trans.comment.service.ToCaseCommentService;
@@ -28,80 +25,80 @@ import com.centaline.trans.utils.BeanUtils;
 /**
  * 
  * @author sstonehu
- * @version $Id: TrackController.java, v 0.1 2016年12月16日 上午1:51:50 sstonehu Exp $
+ * @version $Id: TrackController.java, v 0.1 2016年12月16日 上午1:51:50 sstonehu Exp
+ *          $
  */
 @RestController
 @RequestMapping({ "mobile/track" })
 public class TrackController {
 
-    private static Logger        logger = LoggerFactory.getLogger(MortgageListController.class);
+	@Autowired
+	private ToCaseCommentService toCaseCommentService;
 
-    @Autowired
-    private ToCaseCommentService toCaseCommentService;
+	@Autowired
+	private ToMortgageService toMortgageService;
 
-    @Autowired
-    private ToMortgageService    toMortgageService;
+	@Autowired
+	private StuffService stuffService;
+	@Autowired
+	private TrackService trackService;
 
-    @Autowired
-    private StuffService         stuffService;
-    @Autowired
-    private TrackService trackService;
+	@RequestMapping(value = "/add")
+	@ResponseBody
+	public String addTrack(Model model, @RequestBody CommentVo cmtVo) {
 
-    @RequestMapping(value = "/add")
-    @ResponseBody
-    public String addTrack(Model model, @RequestBody CommentVo cmtVo) {
+		ToCaseComment track = new ToCaseComment();
+		// boolean isNofigyCustomer = cmt.isNotifyCustomer();
+		BeanUtils.copyProperties(cmtVo, track);
 
-        ToCaseComment track = new ToCaseComment();
-        //        boolean isNofigyCustomer = cmt.isNotifyCustomer();
-        BeanUtils.copyProperties(cmtVo, track);
+		// 检查track的完整性。不完整时抛出业务异常
+		this.checkTrackIntegrity(track);
+		int resultCount = 0;
 
-        //检查track的完整性。不完整时抛出业务异常
-        this.checkTrackIntegrity(track);
-        int resultCount = 0;
+		// TODO: 避免switch-case写法，有空的时候修改
+		switch (CommentType.valueOf(track.getType())) {
+		case CMT:
+			resultCount = toCaseCommentService.insertToCaseComment(track);
+			break;
+		case BUJIAN:
+			Boolean isNotifyCustomer = cmtVo.getIsNotifyCustomer() != null ? cmtVo
+					.getIsNotifyCustomer() : false;
+			resultCount = trackService.buJian(track, isNotifyCustomer);
+			break;
+		case REJECT:
+			break;
+		case TRACK:
+			resultCount = toMortgageService.addMortgageTrack4Par(track);
+			break;
+		default:
+			break;
+		}
 
-        //TODO: 避免switch-case写法，有空的时候修改
-        switch (CommentType.valueOf(track.getType())) {
-            case CMT:
-                resultCount = toCaseCommentService.insertToCaseComment(track);
-                break;
-            case BUJIAN:
-                Boolean isNotifyCustomer = cmtVo.getIsNotifyCustomer() != null
-                    ? cmtVo.getIsNotifyCustomer() : false;
-               resultCount=trackService.buJian(track, isNotifyCustomer);
-                break;
-            case REJECT:
-                break;
-            case TRACK:
-                resultCount = toMortgageService.addMortgageTrack4Par(track);
-                break;
-            default:
-                break;
-        }
+		if (resultCount <= 0) {
+			throw new BusinessException("对不起,跟进保存失败");
+		} else {
+			return new StringBuilder("{\"msg\":\"").append(track.getCaseCode())
+					.append("跟进保存成功").append("\"}").toString();
+		}
 
-        if (resultCount <= 0) {
-            throw new BusinessException("对不起,跟进保存失败");
-        } else {
-            return new StringBuilder("{\"msg\":\"").append(track.getCaseCode()).append("跟进保存成功")
-                .append("\"}").toString();
-        }
+	}
 
-    }
+	private boolean checkTrackIntegrity(ToCaseComment track) {
 
-    private boolean checkTrackIntegrity(ToCaseComment track) {
+		if (null == track)
+			throw new BusinessException("抱歉，提交的跟进为空");
+		if (null == track.getSource())
+			throw new BusinessException("抱歉，提交的跟进source为空,请联系技术支持");
+		if (null == track.getType())
+			throw new BusinessException("抱歉，提交的跟进type为空,请联系技术支持");
+		if (null == track.getBizCode())
+			throw new BusinessException("抱歉，提交的跟进bizCode为空,请联系技术支持");
 
-        if (null == track)
-            throw new BusinessException("抱歉，提交的跟进为空");
-        if (null == track.getSource())
-            throw new BusinessException("抱歉，提交的跟进source为空,请联系技术支持");
-        if (null == track.getType())
-            throw new BusinessException("抱歉，提交的跟进type为空,请联系技术支持");
-        if (null == track.getBizCode())
-            throw new BusinessException("抱歉，提交的跟进bizCode为空,请联系技术支持");
+		if (MobileHolder.getMobileUser() != null)
+			track.setCreatorOrgId(MobileHolder.getMobileUser()
+					.getServiceDepId());
 
-        if (MobileHolder.getMobileUser() != null)
-            track.setCreatorOrgId(MobileHolder.getMobileUser().getServiceDepId());
-
-        return false;
-    }
+		return false;
+	}
 
 }
--- trade-mobile/src/test/java/com/centaline/parportal/ParPortalApplicationTests.java
+++ trade-mobile/src/test/java/com/centaline/parportal/ParPortalApplicationTests.java
@@ -2,28 +2,15 @@ package com.centaline.parportal;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.centaline.parportal.mobile.mortgage.web.MortgageController;
-import com.centaline.parportal.mobile.mortgage.web.MortgageListController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ParPortalApplicationTests {
 
-	@Autowired
-	private MortgageListController mortgageListController;
-
-	@Autowired
-	private MortgageController mortgageController;
-
 	@Test
 	public void contextLoads() {
-
-		String result = mortgageListController.caseList(1, 10, null, null,
-				"ff80808158bd58c10158bda37f100020", null);
 
 	}
 
