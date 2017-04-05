4a2890325bbdec48ffc335b1df2579c806218081
yinjf2 <yinjf2@centaline.com.cn>
2017-04-05 14:47:14 +0800

    修改冲突

--- trade-mobile/src/test/java/com/centaline/parportal/ParPortalApplicationTests.java
+++ trade-mobile/src/test/java/com/centaline/parportal/ParPortalApplicationTests.java
@@ -1,5 +1,11 @@
 package com.centaline.parportal;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import com.centaline.parportal.mobile.mortgage.web.MortgageController;
 import com.centaline.parportal.mobile.mortgage.web.MortgageListController;
 
