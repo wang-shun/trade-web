ec78dbf972cce1f2c882b4fa9f2b9da92e6798b6
hejf10 <hejf10@centaline.com.cn>
2017-04-12 10:58:12 +0800

    自建列表

--- trade-web/src/main/java/com/centaline/trans/cases/web/CaseInputController.java
+++ trade-web/src/main/java/com/centaline/trans/cases/web/CaseInputController.java
@@ -2,12 +2,6 @@ package com.centaline.trans.cases.web;
 
 
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 
@@ -15,37 +9,12 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.aist.common.exception.BusinessException;
 import com.aist.common.web.validate.AjaxResponse;
-import com.aist.uam.auth.remote.UamSessionService;
-import com.aist.uam.auth.remote.vo.SessionUser;
-import com.aist.uam.basedata.remote.UamBasedataService;
-import com.aist.uam.template.remote.UamTemplateService;
-import com.aist.uam.userorg.remote.UamUserOrgService;
-import com.aist.uam.userorg.remote.vo.User;
-import com.centaline.trans.cases.entity.ToCase;
-import com.centaline.trans.cases.entity.ToCaseInfo;
 import com.centaline.trans.cases.service.CaseMergeService;
-import com.centaline.trans.cases.service.ToCaseInfoService;
-import com.centaline.trans.cases.service.ToCaseService;
-import com.centaline.trans.cases.vo.CaseMergeVo;
 import com.centaline.trans.common.entity.CaseMergerParameter;
-import com.centaline.trans.common.entity.TgGuestInfo;
-import com.centaline.trans.common.entity.ToPropertyInfo;
-import com.centaline.trans.common.service.PropertyUtilsService;
-import com.centaline.trans.common.service.TgGuestInfoService;
-import com.centaline.trans.common.service.TgServItemAndProcessorService;
-import com.centaline.trans.common.service.ToPropertyInfoService;
-import com.centaline.trans.engine.service.ToWorkFlowService;
-import com.centaline.trans.engine.service.WorkFlowManager;
-import com.centaline.trans.property.service.ToPropertyService;
-import com.centaline.trans.spv.service.ToSpvService;
-import com.centaline.trans.team.service.TsTeamScopeTargetService;
-import com.centaline.trans.utils.DateUtil;
 
 
 /**
