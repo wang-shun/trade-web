45e33ab82816ba5a7f1e9279c6c177a7da17a855
jimmy <504562194@qq.com>
2017-04-14 14:05:08 +0800

    commit

--- yc-par-portal/Dockermake_swagger_edit.sh
+++ yc-par-portal/Dockermake_swagger_edit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #pomver=`mvn help:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
 #pomver=`mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec`
-version=pomver
+version=$pomver
 commit=`git rev-parse --verify --short=8 HEAD`
 branch=`git branch | grep "^\*" | sed -e "s/^\*\ //"`
 #sudo
