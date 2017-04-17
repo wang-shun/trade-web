afb19b097164e04de3c74b9b7e6cfbbabc4cdfcf
[caoyuan7] <caoyuan7@centaline.com.cn>
2017-04-17 17:36:19 +0800

    发布后不显示

--- trade-mobile/src/main/java/com/centaline/parportal/mobile/taskflow/web/PricingController.java
+++ trade-mobile/src/main/java/com/centaline/parportal/mobile/taskflow/web/PricingController.java
@@ -45,7 +45,7 @@ public class PricingController {
     @ResponseBody
     public JSONObject toProcess(String taskId, String processInstanceId) {
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("taskId", taskId);
+        jsonObject.put("taskId", "111");
         jsonObject.put("processInstanceId", processInstanceId);
         return jsonObject;
     }
