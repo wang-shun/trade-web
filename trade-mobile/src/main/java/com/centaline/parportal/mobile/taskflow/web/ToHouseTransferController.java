603931c7feee63d5adb22f23b78eeac3614deba7
[caoyuan7] <caoyuan7@centaline.com.cn>
2017-04-18 10:42:39 +0800

    过户任务详情接口

--- trade-mobile/src/main/java/com/centaline/parportal/mobile/taskflow/web/ToHouseTransferController.java
+++ trade-mobile/src/main/java/com/centaline/parportal/mobile/taskflow/web/ToHouseTransferController.java
@@ -1,20 +1,20 @@
 package com.centaline.parportal.mobile.taskflow.web;
 
 import com.aist.common.web.validate.AjaxResponse;
+
 import com.aist.uam.auth.remote.UamSessionService;
+import com.aist.uam.auth.remote.vo.SessionUser;
+import com.alibaba.fastjson.JSONObject;
 import com.centaline.trans.apilog.service.SalesDealApiService;
-import com.centaline.trans.attachment.service.ToAccesoryListService;
 import com.centaline.trans.cases.entity.ToCase;
-import com.centaline.trans.cases.entity.ToCaseInfo;
-import com.centaline.trans.cases.service.ToCaseInfoService;
 import com.centaline.trans.cases.service.ToCaseService;
+
 import com.centaline.trans.common.enums.CaseMergeStatusEnum;
 import com.centaline.trans.common.service.TgGuestInfoService;
-import com.centaline.trans.engine.bean.RestVariable;
+
 import com.centaline.trans.mortgage.entity.ToMortgage;
 import com.centaline.trans.mortgage.service.ToMortgageService;
 import com.centaline.trans.task.entity.ToHouseTransfer;
-import com.centaline.trans.task.entity.ToPricing;
 import com.centaline.trans.task.service.ToHouseTransferService;
 import com.centaline.trans.task.vo.LoanlostApproveVO;
 import org.apache.log4j.Logger;
@@ -22,6 +22,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by caoyuan7 on 2017/4/14.
@@ -44,9 +46,29 @@ public class ToHouseTransferController {
     @Autowired
     private SalesDealApiService salesdealApiService;
 
+    @Autowired
+    private ToMortgageService toMortgageService;
+
+    @Autowired
+    private UamSessionService uamSessionService;
+
+    @RequestMapping(value = "process")
+    @ResponseBody
+    public JSONObject toProcess(HttpServletRequest request, String processInstanceId) {
+        String taskId = request.getParameter("taskId");
+        SessionUser user= uamSessionService.getSessionUser();
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("taskId", taskId);
+        jsonObject.put("processInstanceId", processInstanceId);
+        jsonObject.put("approveType", "2");
+        jsonObject.put("operator", user != null ? user.getId() : "");
+        jsonObject.put("partCode", "Guohu");
+        return jsonObject;
+    }
+
     @RequestMapping(value = "submitToHouseTransfer")
     @ResponseBody
-    public AjaxResponse submitToHouseTransfer(ToHouseTransfer toHouseTransfer,ToMortgage toMortgage,LoanlostApproveVO loanlostApproveVO,String taskId,String processInstanceId) {
+    public AjaxResponse submitToHouseTransfer(ToHouseTransfer toHouseTransfer,LoanlostApproveVO loanlostApproveVO,String taskId,String processInstanceId) {
         AjaxResponse<?> response = new AjaxResponse<>();
         try {
             ToCase toCase = toCaseService.findToCaseByCaseCode(toHouseTransfer.getCaseCode());
@@ -61,6 +83,8 @@ public class ToHouseTransferController {
                     response.setMessage("自建案件必须完成案件合流才能提交过户申请");
                     return response;
                 }
+
+                ToMortgage toMortgage = toMortgageService.findToMortgageByCaseCode2(toHouseTransfer.getCaseCode());
                 toHouseTransferService.submitToHouseTransfer(toHouseTransfer, toMortgage, loanlostApproveVO, taskId, processInstanceId);
                 // 回写三级市场, 交易过户
                 salesdealApiService.noticeSalesDeal(toCase.getCtmCode());
