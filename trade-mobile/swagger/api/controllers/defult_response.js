95448519f4149b0c081722b5d264eaeed305c5b0
jimmy <504562194@qq.com>
2017-04-21 15:47:36 +0800

    添加查询dict接口

--- trade-mobile/swagger/api/controllers/defult_response.js
+++ trade-mobile/swagger/api/controllers/defult_response.js
@@ -29,7 +29,8 @@ module.exports = {
   responseJSON: responseJSON,
   contactRsponse: responseJSON,
   casecontactRsponse: responseJSON,
-  pricingRsponse: responseJSON
+  pricingRsponse: responseJSON,
+  dictListResponse: responseJSON
 };
 
 /*
