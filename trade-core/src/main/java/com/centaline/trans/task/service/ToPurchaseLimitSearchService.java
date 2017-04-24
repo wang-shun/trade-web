8e613f3ab493fa9e094b81922877b278871cbe14
[caoyuan7] <caoyuan7@centaline.com.cn>
2017-04-24 10:52:54 +0800

    增加service事务

--- trade-core/src/main/java/com/centaline/trans/task/service/ToPurchaseLimitSearchService.java
+++ trade-core/src/main/java/com/centaline/trans/task/service/ToPurchaseLimitSearchService.java
@@ -1,5 +1,6 @@
 package com.centaline.trans.task.service;
 
+import com.aist.common.web.validate.AjaxResponse;
 import com.centaline.trans.task.entity.ToPurchaseLimitSearch;
 
 public interface ToPurchaseLimitSearchService {
@@ -7,4 +8,6 @@ public interface ToPurchaseLimitSearchService {
 	public boolean saveToPurchaseLimitSearch(ToPurchaseLimitSearch toPurchaseLimitSearch);
 	
 	public ToPurchaseLimitSearch findToPlsByCaseCode(String caseCode);
+
+	AjaxResponse saveAndSubmitPurchaseLimit(ToPurchaseLimitSearch toPurchaseLimitSearch,String taskId,String processInstanceId);
 }
--- trade-core/src/main/java/com/centaline/trans/task/service/impl/ToPurchaseLimitSearchServiceImpl.java
+++ trade-core/src/main/java/com/centaline/trans/task/service/impl/ToPurchaseLimitSearchServiceImpl.java
@@ -1,5 +1,11 @@
 package com.centaline.trans.task.service.impl;
 
+import com.aist.common.web.validate.AjaxResponse;
+import com.centaline.trans.cases.entity.ToCase;
+import com.centaline.trans.cases.service.ToCaseService;
+import com.centaline.trans.common.service.TgGuestInfoService;
+import com.centaline.trans.engine.bean.RestVariable;
+import com.centaline.trans.engine.service.WorkFlowManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,12 +13,23 @@ import com.centaline.trans.task.entity.ToPurchaseLimitSearch;
 import com.centaline.trans.task.repository.ToPurchaseLimitSearchMapper;
 import com.centaline.trans.task.service.ToPurchaseLimitSearchService;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ToPurchaseLimitSearchServiceImpl implements ToPurchaseLimitSearchService {
 
 	@Autowired
 	private ToPurchaseLimitSearchMapper toPurchaseLimitSearchMapper;
-	
+
+	@Autowired(required = true)
+	private ToCaseService toCaseService;
+	@Autowired
+	private WorkFlowManager workFlowManager;
+
+	@Autowired
+	private TgGuestInfoService tgGuestInfoService;
+
 	@Override
 	public boolean saveToPurchaseLimitSearch( ToPurchaseLimitSearch toPurchaseLimitSearch) {
 		if(toPurchaseLimitSearch.getCaseCode()==null || toPurchaseLimitSearch.getCaseCode().intern().length() == 0) {
@@ -35,6 +52,29 @@ public class ToPurchaseLimitSearchServiceImpl implements ToPurchaseLimitSearchSe
 	@Override
 	public ToPurchaseLimitSearch findToPlsByCaseCode(String caseCode) {
 		return toPurchaseLimitSearchMapper.findToPlsByCaseCode(caseCode);
+	}
+
+	@Override
+	public AjaxResponse saveAndSubmitPurchaseLimit(ToPurchaseLimitSearch toPurchaseLimitSearch, String taskId, String processInstanceId) {
+		AjaxResponse ajaxResponse = new AjaxResponse();
+		Boolean saveFlag = saveToPurchaseLimitSearch(toPurchaseLimitSearch);
+
+		if(saveFlag){
+			List<RestVariable> variables = new ArrayList<RestVariable>();
+			ToCase toCase = toCaseService.findToCaseByCaseCode(toPurchaseLimitSearch.getCaseCode());
+			workFlowManager.submitTask(variables, taskId, processInstanceId,toCase.getLeadingProcessId(),toPurchaseLimitSearch.getCaseCode());
+			int result = tgGuestInfoService.sendMsgHistory(toPurchaseLimitSearch.getCaseCode(),toPurchaseLimitSearch.getPartId());
+			if (result >0) {
+				ajaxResponse.setMessage("查限购保存成功");
+			}else {
+				ajaxResponse.setMessage("短信发送失败, 请您线下手工再次发送！");
+			}
+			ajaxResponse.setSuccess(true);
+		} else {
+			ajaxResponse.setSuccess(false);
+			ajaxResponse.setMessage("保存查限购出错");
+		}
+		return ajaxResponse;
 	}
 
 }
