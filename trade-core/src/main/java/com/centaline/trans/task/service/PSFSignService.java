4dc5ed3671d46917574ca675dbfb55b38c53a55f
[caoyuan7] <caoyuan7@centaline.com.cn>
2017-04-25 10:26:29 +0800

    公积金审批和签约

--- trade-core/src/main/java/com/centaline/trans/task/service/PSFSignService.java
+++ trade-core/src/main/java/com/centaline/trans/task/service/PSFSignService.java
@@ -1,5 +1,6 @@
 package com.centaline.trans.task.service;
 
+import com.aist.common.web.validate.AjaxResponse;
 import com.centaline.trans.task.vo.PSFSignVO;
 
 public interface PSFSignService {
@@ -7,6 +8,10 @@ public interface PSFSignService {
 	public Boolean savePSFSign(PSFSignVO psfSignVO);
 	
 	public PSFSignVO queryPSFSign(String caseCode);
-	
+
 	public PSFSignVO queryPSFSignNoBlank(String caseCode);
+
+	AjaxResponse saveAndSubmitPSFSign(PSFSignVO psfSignVO);
+
+
 }
