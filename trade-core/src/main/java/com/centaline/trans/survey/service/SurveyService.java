b9c9e283109e8209cbf53d928f8945a8ed328759
gongjd <gongjd@centaline.com.cn>
2017-04-27 10:01:49 +0800

    客服回访流程图
    Change-Id: I1306edac171ee3484a4b9cea56d26db841835d5a
    Signed-off-by: gongjd <gongjd@centaline.com.cn>

--- trade-core/src/main/java/com/centaline/trans/survey/service/SurveyService.java
+++ trade-core/src/main/java/com/centaline/trans/survey/service/SurveyService.java
@@ -1,5 +1,17 @@
 package com.centaline.trans.survey.service;
 
+import java.util.List;
+
+import com.centaline.trans.survey.vo.SurveyVO;
+
 public interface SurveyService {
+
+	int updateSurveyVO(SurveyVO surveyVO);
+	
+	int insertSurveyVO(SurveyVO surveyVO);
+	
+	List<SurveyVO> querySurveyVOList();
+	
+	SurveyVO querySurveyVOById(int id);
 
 }
--- trade-web/src/main/java/com/centaline/trans/survey/web/SurveyController.java
+++ trade-web/src/main/java/com/centaline/trans/survey/web/SurveyController.java
@@ -1,11 +1,15 @@
 package com.centaline.trans.survey.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.aist.common.web.validate.AjaxResponse;
 import com.centaline.trans.survey.service.SurveyService;
+import com.centaline.trans.survey.vo.SurveyVO;
 
 @Controller
 @RequestMapping("survey")
@@ -16,39 +20,77 @@ public class SurveyController {
 	
 	//页面
 	@RequestMapping("/list")
-	public String list(){
+	public String list(Model model){
+		List<SurveyVO> surveyVOList = surveyService.querySurveyVOList();
+		model.addAttribute("surveyVOList", surveyVOList);
 		return "";
 	}
 	
 	@RequestMapping("/detail")
-	public String detail(){
+	public String detail(Model model, int surveyId){
+		SurveyVO surveyVO = surveyService.querySurveyVOById(surveyId);
+		model.addAttribute("survey", surveyVO);
 		return "";
 	}
 	
 	@RequestMapping("/edit")
-	public String edit(){
+	public String edit(Model model, int surveyId){
+		SurveyVO surveyVO = surveyService.querySurveyVOById(surveyId);
+		model.addAttribute("survey", surveyVO);
 		return "";
 	}
 	
 	//操作
 	@RequestMapping("dispatch")
 	public AjaxResponse<String> dispatch(){
-		return new AjaxResponse<String>();
+		AjaxResponse<String> response = new AjaxResponse<String>();
+		try{
+			response.setSuccess(true);
+			response.setMessage("操作成功！");
+		}catch(Exception e){
+			response.setSuccess(false);
+			response.setMessage(e.getMessage());
+		}
+		return response;
 	}
 	
 	@RequestMapping("pass")
 	public AjaxResponse<String> surveyPass(){
-		return new AjaxResponse<String>();
+		AjaxResponse<String> response = new AjaxResponse<String>();
+		try{
+			response.setSuccess(true);
+			response.setMessage("操作成功！");
+		}catch(Exception e){
+			response.setSuccess(false);
+			response.setMessage(e.getMessage());
+		}
+		return response;
 	}
 	
 	@RequestMapping("reject")
 	public AjaxResponse<String> surveyReject(){
-		return new AjaxResponse<String>();
+		AjaxResponse<String> response = new AjaxResponse<String>();
+		try{
+			response.setSuccess(true);
+			response.setMessage("操作成功！");
+		}catch(Exception e){
+			response.setSuccess(false);
+			response.setMessage(e.getMessage());
+		}
+		return response;
 	}
 	
 	@RequestMapping("submitEdit")
 	public AjaxResponse<String> submitEdit(){
-		return new AjaxResponse<String>();
+		AjaxResponse<String> response = new AjaxResponse<String>();
+		try{
+			response.setSuccess(true);
+			response.setMessage("操作成功！");
+		}catch(Exception e){
+			response.setSuccess(false);
+			response.setMessage(e.getMessage());
+		}
+		return response;
 	}
 
 }
