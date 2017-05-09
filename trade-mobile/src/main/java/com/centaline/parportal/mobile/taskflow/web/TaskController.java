ed47c52e9b0ff36f69b65fbf908314b76493c749
[caoyuan7] <caoyuan7@centaline.com.cn>
2017-05-09 14:52:12 +0800

    修改app端bug

--- trade-mobile/src/main/java/com/centaline/parportal/mobile/taskflow/web/TaskController.java
+++ trade-mobile/src/main/java/com/centaline/parportal/mobile/taskflow/web/TaskController.java
@@ -116,7 +116,7 @@ public class TaskController {
 			List<User> asList = uamUserOrgService.getUserByOrgIdAndJobCode(orgId, TransJobs.TJYZL.getCode());
 			if (CollectionUtils.isNotEmpty(asList)) {
 				User user = asList.get(0);
-				zhuli = user.getUsername();
+				zhuli = user.getRealName();
 			}
 			map.put("zhuli", zhuli);
 		}
