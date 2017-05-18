5bb42ebf6501901228e798d346a09ce1be9c2ad7
zhuody <zhuody@centaline.com.cn>
2017-05-18 19:57:34 +0800

    xml

---
+++ trade-core/src/main/java/com/centaline/trans/award/entity/TsAwardCaseCental.java
@@ -0,0 +1,415 @@
+package com.centaline.trans.award.entity;
+
+import java.util.Date;
+
+public class TsAwardCaseCental {
+    private Long pkid;
+
+    private String caseCode;
+
+    private String frontLeadingProcess;
+
+    private String frontOrgId;
+
+    private String frontDistrictId;
+
+    private String frontManager;
+
+    private String frontSeniorManager;
+
+    private String frontDirector;
+
+    private String frontTeamAssistant;
+
+    private String frontOperationsManager;
+
+    private String frontGeneralManager;
+
+    private String frontManagerOrgId;
+
+    private String frontSeniorManagerOrgId;
+
+    private String frontDirectorOrgId;
+
+    private String frontTeamAssistantOrgId;
+
+    private String frontOperationsManagerOrgId;
+
+    private String frontGeneralManagerOrgId;
+
+    private String backLeadingProcess;
+
+    private String backOrgId;
+
+    private String backDistrictId;
+
+    private String backManager;
+
+    private String backSeniorManager;
+
+    private String backDirector;
+
+    private String backTeamAssistant;
+
+    private String backOperationsManager;
+
+    private String backGeneralManager;
+
+    private String backManagerOrgId;
+
+    private String backSeniorManagerOrgId;
+
+    private String backDirectorOrgId;
+
+    private String backTeamAssistantOrgId;
+
+    private String backOperationsManagerOrgId;
+
+    private String backGeneralManagerOrgId;
+
+    private Date guohuApproveTime;
+
+    private String guohuApproveRecord;
+
+    private String awardStatus;
+
+    private Date awardMonth;
+
+    private String awardDesc;
+
+    private String createBy;
+
+    private Date createTime;
+
+    private String updateBy;
+
+    private Date updateTime;
+
+    public Long getPkid() {
+        return pkid;
+    }
+
+    public void setPkid(Long pkid) {
+        this.pkid = pkid;
+    }
+
+    public String getCaseCode() {
+        return caseCode;
+    }
+
+    public void setCaseCode(String caseCode) {
+        this.caseCode = caseCode == null ? null : caseCode.trim();
+    }
+
+    public String getFrontLeadingProcess() {
+        return frontLeadingProcess;
+    }
+
+    public void setFrontLeadingProcess(String frontLeadingProcess) {
+        this.frontLeadingProcess = frontLeadingProcess == null ? null : frontLeadingProcess.trim();
+    }
+
+    public String getFrontOrgId() {
+        return frontOrgId;
+    }
+
+    public void setFrontOrgId(String frontOrgId) {
+        this.frontOrgId = frontOrgId == null ? null : frontOrgId.trim();
+    }
+
+    public String getFrontDistrictId() {
+        return frontDistrictId;
+    }
+
+    public void setFrontDistrictId(String frontDistrictId) {
+        this.frontDistrictId = frontDistrictId == null ? null : frontDistrictId.trim();
+    }
+
+    public String getFrontManager() {
+        return frontManager;
+    }
+
+    public void setFrontManager(String frontManager) {
+        this.frontManager = frontManager == null ? null : frontManager.trim();
+    }
+
+    public String getFrontSeniorManager() {
+        return frontSeniorManager;
+    }
+
+    public void setFrontSeniorManager(String frontSeniorManager) {
+        this.frontSeniorManager = frontSeniorManager == null ? null : frontSeniorManager.trim();
+    }
+
+    public String getFrontDirector() {
+        return frontDirector;
+    }
+
+    public void setFrontDirector(String frontDirector) {
+        this.frontDirector = frontDirector == null ? null : frontDirector.trim();
+    }
+
+    public String getFrontTeamAssistant() {
+        return frontTeamAssistant;
+    }
+
+    public void setFrontTeamAssistant(String frontTeamAssistant) {
+        this.frontTeamAssistant = frontTeamAssistant == null ? null : frontTeamAssistant.trim();
+    }
+
+    public String getFrontOperationsManager() {
+        return frontOperationsManager;
+    }
+
+    public void setFrontOperationsManager(String frontOperationsManager) {
+        this.frontOperationsManager = frontOperationsManager == null ? null : frontOperationsManager.trim();
+    }
+
+    public String getFrontGeneralManager() {
+        return frontGeneralManager;
+    }
+
+    public void setFrontGeneralManager(String frontGeneralManager) {
+        this.frontGeneralManager = frontGeneralManager == null ? null : frontGeneralManager.trim();
+    }
+
+    public String getFrontManagerOrgId() {
+        return frontManagerOrgId;
+    }
+
+    public void setFrontManagerOrgId(String frontManagerOrgId) {
+        this.frontManagerOrgId = frontManagerOrgId == null ? null : frontManagerOrgId.trim();
+    }
+
+    public String getFrontSeniorManagerOrgId() {
+        return frontSeniorManagerOrgId;
+    }
+
+    public void setFrontSeniorManagerOrgId(String frontSeniorManagerOrgId) {
+        this.frontSeniorManagerOrgId = frontSeniorManagerOrgId == null ? null : frontSeniorManagerOrgId.trim();
+    }
+
+    public String getFrontDirectorOrgId() {
+        return frontDirectorOrgId;
+    }
+
+    public void setFrontDirectorOrgId(String frontDirectorOrgId) {
+        this.frontDirectorOrgId = frontDirectorOrgId == null ? null : frontDirectorOrgId.trim();
+    }
+
+    public String getFrontTeamAssistantOrgId() {
+        return frontTeamAssistantOrgId;
+    }
+
+    public void setFrontTeamAssistantOrgId(String frontTeamAssistantOrgId) {
+        this.frontTeamAssistantOrgId = frontTeamAssistantOrgId == null ? null : frontTeamAssistantOrgId.trim();
+    }
+
+    public String getFrontOperationsManagerOrgId() {
+        return frontOperationsManagerOrgId;
+    }
+
+    public void setFrontOperationsManagerOrgId(String frontOperationsManagerOrgId) {
+        this.frontOperationsManagerOrgId = frontOperationsManagerOrgId == null ? null : frontOperationsManagerOrgId.trim();
+    }
+
+    public String getFrontGeneralManagerOrgId() {
+        return frontGeneralManagerOrgId;
+    }
+
+    public void setFrontGeneralManagerOrgId(String frontGeneralManagerOrgId) {
+        this.frontGeneralManagerOrgId = frontGeneralManagerOrgId == null ? null : frontGeneralManagerOrgId.trim();
+    }
+
+    public String getBackLeadingProcess() {
+        return backLeadingProcess;
+    }
+
+    public void setBackLeadingProcess(String backLeadingProcess) {
+        this.backLeadingProcess = backLeadingProcess == null ? null : backLeadingProcess.trim();
+    }
+
+    public String getBackOrgId() {
+        return backOrgId;
+    }
+
+    public void setBackOrgId(String backOrgId) {
+        this.backOrgId = backOrgId == null ? null : backOrgId.trim();
+    }
+
+    public String getBackDistrictId() {
+        return backDistrictId;
+    }
+
+    public void setBackDistrictId(String backDistrictId) {
+        this.backDistrictId = backDistrictId == null ? null : backDistrictId.trim();
+    }
+
+    public String getBackManager() {
+        return backManager;
+    }
+
+    public void setBackManager(String backManager) {
+        this.backManager = backManager == null ? null : backManager.trim();
+    }
+
+    public String getBackSeniorManager() {
+        return backSeniorManager;
+    }
+
+    public void setBackSeniorManager(String backSeniorManager) {
+        this.backSeniorManager = backSeniorManager == null ? null : backSeniorManager.trim();
+    }
+
+    public String getBackDirector() {
+        return backDirector;
+    }
+
+    public void setBackDirector(String backDirector) {
+        this.backDirector = backDirector == null ? null : backDirector.trim();
+    }
+
+    public String getBackTeamAssistant() {
+        return backTeamAssistant;
+    }
+
+    public void setBackTeamAssistant(String backTeamAssistant) {
+        this.backTeamAssistant = backTeamAssistant == null ? null : backTeamAssistant.trim();
+    }
+
+    public String getBackOperationsManager() {
+        return backOperationsManager;
+    }
+
+    public void setBackOperationsManager(String backOperationsManager) {
+        this.backOperationsManager = backOperationsManager == null ? null : backOperationsManager.trim();
+    }
+
+    public String getBackGeneralManager() {
+        return backGeneralManager;
+    }
+
+    public void setBackGeneralManager(String backGeneralManager) {
+        this.backGeneralManager = backGeneralManager == null ? null : backGeneralManager.trim();
+    }
+
+    public String getBackManagerOrgId() {
+        return backManagerOrgId;
+    }
+
+    public void setBackManagerOrgId(String backManagerOrgId) {
+        this.backManagerOrgId = backManagerOrgId == null ? null : backManagerOrgId.trim();
+    }
+
+    public String getBackSeniorManagerOrgId() {
+        return backSeniorManagerOrgId;
+    }
+
+    public void setBackSeniorManagerOrgId(String backSeniorManagerOrgId) {
+        this.backSeniorManagerOrgId = backSeniorManagerOrgId == null ? null : backSeniorManagerOrgId.trim();
+    }
+
+    public String getBackDirectorOrgId() {
+        return backDirectorOrgId;
+    }
+
+    public void setBackDirectorOrgId(String backDirectorOrgId) {
+        this.backDirectorOrgId = backDirectorOrgId == null ? null : backDirectorOrgId.trim();
+    }
+
+    public String getBackTeamAssistantOrgId() {
+        return backTeamAssistantOrgId;
+    }
+
+    public void setBackTeamAssistantOrgId(String backTeamAssistantOrgId) {
+        this.backTeamAssistantOrgId = backTeamAssistantOrgId == null ? null : backTeamAssistantOrgId.trim();
+    }
+
+    public String getBackOperationsManagerOrgId() {
+        return backOperationsManagerOrgId;
+    }
+
+    public void setBackOperationsManagerOrgId(String backOperationsManagerOrgId) {
+        this.backOperationsManagerOrgId = backOperationsManagerOrgId == null ? null : backOperationsManagerOrgId.trim();
+    }
+
+    public String getBackGeneralManagerOrgId() {
+        return backGeneralManagerOrgId;
+    }
+
+    public void setBackGeneralManagerOrgId(String backGeneralManagerOrgId) {
+        this.backGeneralManagerOrgId = backGeneralManagerOrgId == null ? null : backGeneralManagerOrgId.trim();
+    }
+
+    public Date getGuohuApproveTime() {
+        return guohuApproveTime;
+    }
+
+    public void setGuohuApproveTime(Date guohuApproveTime) {
+        this.guohuApproveTime = guohuApproveTime;
+    }
+
+    public String getGuohuApproveRecord() {
+        return guohuApproveRecord;
+    }
+
+    public void setGuohuApproveRecord(String guohuApproveRecord) {
+        this.guohuApproveRecord = guohuApproveRecord == null ? null : guohuApproveRecord.trim();
+    }
+
+    public String getAwardStatus() {
+        return awardStatus;
+    }
+
+    public void setAwardStatus(String awardStatus) {
+        this.awardStatus = awardStatus == null ? null : awardStatus.trim();
+    }
+
+    public Date getAwardMonth() {
+        return awardMonth;
+    }
+
+    public void setAwardMonth(Date awardMonth) {
+        this.awardMonth = awardMonth;
+    }
+
+    public String getAwardDesc() {
+        return awardDesc;
+    }
+
+    public void setAwardDesc(String awardDesc) {
+        this.awardDesc = awardDesc == null ? null : awardDesc.trim();
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy == null ? null : createBy.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy == null ? null : updateBy.trim();
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
