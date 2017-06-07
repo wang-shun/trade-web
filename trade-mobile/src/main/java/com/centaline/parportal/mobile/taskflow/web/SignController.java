0d642d52f994b9e1c16f0075079ce905b56ea7f5
[caoyuan7] <caoyuan7@centaline.com.cn>
2017-06-07 13:28:09 +0800

    案件导出加是否首套字段

--- trade-mobile/src/main/java/com/centaline/parportal/mobile/taskflow/web/SignController.java
+++ trade-mobile/src/main/java/com/centaline/parportal/mobile/taskflow/web/SignController.java
@@ -1,29 +1,21 @@
 package com.centaline.parportal.mobile.taskflow.web;
 
-import com.aist.message.core.remote.UamMessageService;
 import com.aist.uam.permission.remote.UamPermissionService;
 import com.aist.uam.permission.remote.vo.App;
 import com.alibaba.fastjson.JSONObject;
 import com.centaline.trans.attachment.service.ToAccesoryListService;
 import com.centaline.trans.cases.entity.Result2;
-import com.centaline.trans.cases.service.ToCaseService;
-import com.centaline.trans.cases.vo.CaseBaseVO;
 import com.centaline.trans.common.entity.TgGuestInfo;
 import com.centaline.trans.common.enums.AppTypeEnum;
 import com.centaline.trans.common.service.TgGuestInfoService;
-import com.centaline.trans.mortgage.service.ToMortgageService;
 import com.centaline.trans.task.service.SignService;
-import com.centaline.trans.task.service.ToHouseTransferService;
 import com.centaline.trans.task.vo.TransSignVO;
-import com.centaline.trans.utils.UiImproveUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
@@ -37,22 +29,11 @@ public class SignController {
     private SignService signService;
 
     @Autowired
-    private ToMortgageService toMortgageService;
-
-    @Autowired(required = true)
-    private ToCaseService toCaseService;
-
-    @Autowired
     private TgGuestInfoService tgGuestInfoService;
 
-    @Qualifier("uamMessageServiceClient")
-    @Autowired
-    private UamMessageService uamMessageService;
     @Autowired
     private ToAccesoryListService toAccesoryListService;
 
-    @Autowired
-    private ToHouseTransferService toHouseTransferService;
     @Autowired
     private UamPermissionService uamPermissionService;
 
