8b2338b44c5109038411be5f84b7a15ecf017e4f
yinjf2 <yinjf2@centaline.com.cn>
2017-06-28 15:50:28 +0800

    案件时效controller、service、mapper类添加注释

--- trade-web/src/main/java/com/centaline/trans/cases/web/CaseEfficientController.java
+++ trade-web/src/main/java/com/centaline/trans/cases/web/CaseEfficientController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.aist.uam.auth.remote.UamSessionService;
 import com.aist.uam.auth.remote.vo.SessionUser;
 import com.centaline.trans.cases.service.TsCaseEfficientService;
-import com.centaline.trans.comment.service.ToCaseCommentService;
 
 /**
  * 案件时效管理控制层
@@ -21,50 +20,56 @@ import com.centaline.trans.comment.service.ToCaseCommentService;
  */
 @Controller
 @RequestMapping(value = "caseEfficient")
-public class CaseEfficientController
-{
+public class CaseEfficientController {
 
-    @Autowired
-    private UamSessionService uamSessionService;
+	@Autowired
+	private UamSessionService uamSessionService;
 
-    @Autowired
-    private ToCaseCommentService toCaseCommentService;
+	@Autowired
+	private TsCaseEfficientService tsCaseEfficientService;
 
-    @Autowired
-    private TsCaseEfficientService tsCaseEfficientService;
+	/**
+	 * 进入案件时效管理列表
+	 * 
+	 * @param model
+	 * @param request
+	 * @return 返回案件时效列表信息
+	 */
+	@RequestMapping(value = "list")
+	public String list(Model model, ServletRequest request) {
+		SessionUser currentUser = uamSessionService.getSessionUser();
+		String serviceDepId = currentUser.getServiceDepId();
 
-    /**
-     * 进入案件时效管理列表
-     * 
-     * @param model
-     * @param request
-     * @return 返回案件时效列表信息
-     */
-    @RequestMapping(value = "list")
-    public String list(Model model, ServletRequest request)
-    {
-        SessionUser currentUser = uamSessionService.getSessionUser();
-        String serviceDepId = currentUser.getServiceDepId();
+		// 运营部开放所有权限
+		if ("YCYYZY".equals(currentUser.getServiceJobCode()) || "YCYYZG".equals(currentUser.getServiceJobCode())) {
+			serviceDepId = "ff8080814f459a78014f45a73d820006";
+		}
 
-        // 运营部开放所有权限
-        if ("YCYYZY".equals(currentUser.getServiceJobCode()) || "YCYYZG".equals(currentUser.getServiceJobCode()))
-        {
-            serviceDepId = "ff8080814f459a78014f45a73d820006";
-        }
+		request.setAttribute("serviceDepId", serviceDepId);
+		request.setAttribute("currentUser", currentUser);
 
-        request.setAttribute("serviceDepId", serviceDepId);
-        request.setAttribute("currentUser", currentUser);
+		return "case/caseEfficientList";
+	}
 
-        return "case/caseEfficientList";
-    }
+	/**
+	 * 延期
+	 * 
+	 * @param caseCode
+	 *            案件编号
+	 * @param partCode
+	 *            环节编号
+	 * @param delayDays
+	 *            延期天数
+	 * @param comment
+	 *            延期原因
+	 * @return 返回true,延期成功;返回false,延期失败。
+	 */
+	@RequestMapping(value = "delay")
+	@ResponseBody
+	public boolean delay(String caseCode, String partCode, String delayDays, String comment) {
+		SessionUser currentUser = uamSessionService.getSessionUser();
 
-    @RequestMapping(value = "delay")
-    @ResponseBody
-    public boolean delay(String caseCode, String partCode, String delayDays, String comment)
-    {
-        SessionUser currentUser = uamSessionService.getSessionUser();
-
-        return tsCaseEfficientService.delay(currentUser, caseCode, partCode, delayDays, comment);
-    }
+		return tsCaseEfficientService.delay(currentUser, caseCode, partCode, delayDays, comment);
+	}
 
 }
