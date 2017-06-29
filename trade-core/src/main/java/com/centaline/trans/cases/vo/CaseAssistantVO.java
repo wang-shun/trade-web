6870e6c7caa82e199f87190b9b8c4e96206f4997
mouwm1 <mouwm1@centaline.com.cn>
2017-06-29 11:17:05 +0800

    添加助理实体类

---
+++ trade-core/src/main/java/com/centaline/trans/cases/vo/CaseAssistantVO.java
@@ -0,0 +1,36 @@
+package com.centaline.trans.cases.vo;
+
+public class CaseAssistantVO {
+	
+	private String userId;
+	
+	private String userRealName;
+	
+	private String userPhone;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserRealName() {
+		return userRealName;
+	}
+
+	public void setUserRealName(String userRealName) {
+		this.userRealName = userRealName;
+	}
+
+	public String getUserPhone() {
+		return userPhone;
+	}
+
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+	
+
+}
