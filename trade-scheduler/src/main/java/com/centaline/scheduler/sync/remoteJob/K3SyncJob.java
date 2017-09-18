b15542faeb619ad1d5e2cb26945e21c6df0c6ddf
yinchao <yinchao@centaline.com.cn>
2017-09-18 20:52:11 +0800

    修复人事数据调度 引起的先同步的城市 数据失效问题

--- trade-scheduler/src/main/java/com/centaline/scheduler/sync/remoteJob/K3SyncJob.java
+++ trade-scheduler/src/main/java/com/centaline/scheduler/sync/remoteJob/K3SyncJob.java
@@ -4,6 +4,8 @@ import com.aist.scheduler.execution.remote.Job;
 import com.aist.scheduler.execution.remote.vo.JobExecutionContext;
 import com.centaline.scheduler.sync.enums.CityType;
 import com.centaline.scheduler.sync.service.SyncHrService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -17,7 +19,7 @@ import org.springframework.stereotype.Component;
  */
 @Component  //让spring加入bean管理中 否则手动配置
 public class K3SyncJob implements Job{
-
+	private Logger logger = LoggerFactory.getLogger(K3SyncJob.class);
 	@Autowired
 	@Qualifier("bJSyncService")
 	SyncHrService bJSyncService;
@@ -28,7 +30,7 @@ public class K3SyncJob implements Job{
 	@Override
 	public void execute(JobExecutionContext context) {
 		//注意顺序 后面不同城市 先在该处 进行添加 后面再进行调整
-		System.out.println("-----------execu K3Sync Job-----start-----");
+		logger.info("-----------execu K3Sync Job-----start-----");
 		//北京
 		bJSyncService.toSyncOrgUnit(CityType.BEIJING,"AUTO");
 		bJSyncService.toSyncEmployee(CityType.BEIJING,"AUTO");
@@ -39,6 +41,11 @@ public class K3SyncJob implements Job{
 		tJSyncService.toSyncEmployee(CityType.TIANJIN,"AUTO");
 		tJSyncService.toSyncPosition(CityType.TIANJIN,"AUTO");
 		tJSyncService.toSyncEmpOrgPos(CityType.TIANJIN,"AUTO");
-		System.out.println("-----------execu K3Sync Job-----end -----");
+		//执行存储过程 同步到正式表中 service用任何城市的都可以 只要没有重写即可
+		bJSyncService.doProcExec("{call sync.proc_sync_dept(?,?)}");
+		bJSyncService.doProcExec("{call sync.proc_sync_emp(?,?)}");
+		bJSyncService.doProcExec("{call sync.proc_sync_position(?,?)}");
+		bJSyncService.doProcExec("{call sync.proc_sync_eop(?,?)}");
+		logger.info("-----------execu K3Sync Job-----end -----");
 	}
 }
