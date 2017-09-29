af0f64d2b400743c2251291299555fbbc24d5d1f
jinwl6 <jinwl6@centaline.com.cn>
2017-09-29 21:01:54 +0800

    解决冲突

--- trade-core/src/main/java/com/centaline/trans/ransom/service/AddRansomFormService.java
+++ trade-core/src/main/java/com/centaline/trans/ransom/service/AddRansomFormService.java
@@ -2,19 +2,10 @@ package com.centaline.trans.ransom.service;
 
 import java.util.List;
 
-<<<<<<< Upstream, based on origin/develop
-import com.centaline.trans.ransom.entity.AddRansomForm;
-
-public interface AddRansomFormService {
-	
-	int insert(AddRansomForm ar);
-	
-	void addRansomForm(List<AddRansomForm> list);
-=======
 import com.centaline.trans.ransom.entity.ToRansomFormVo;
 
 public interface AddRansomFormService {
 	
 	void addRansomForm(List<ToRansomFormVo> list);
->>>>>>> 119371f 解决冲突
 }
+
