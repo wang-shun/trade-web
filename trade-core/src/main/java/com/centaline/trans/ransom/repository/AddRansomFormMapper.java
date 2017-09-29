60b2107061df7cb762e0598271ddac6b5f0173f7
jinwl6 <jinwl6@centaline.com.cn>
2017-09-29 21:42:46 +0800

    解决冲突

--- trade-core/src/main/java/com/centaline/trans/ransom/repository/AddRansomFormMapper.java
+++ trade-core/src/main/java/com/centaline/trans/ransom/repository/AddRansomFormMapper.java
@@ -3,21 +3,10 @@ package com.centaline.trans.ransom.repository;
 import java.util.List;
 
 import com.centaline.trans.common.MyBatisRepository;
-<<<<<<< Upstream, based on origin/develop
-import com.centaline.trans.ransom.entity.AddRansomForm;
-
-@MyBatisRepository
-public interface AddRansomFormMapper {
-
-	int insert(AddRansomForm ar);
-
-	void addRansomForm(List<AddRansomForm> list);
-=======
 import com.centaline.trans.ransom.entity.ToRansomFormVo;
 
 @MyBatisRepository
 public interface AddRansomFormMapper {
 
 	void addRansomForm(List<ToRansomFormVo> list);
->>>>>>> 119371f 解决冲突
 }
