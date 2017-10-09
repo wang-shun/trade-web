47a09430cb651e1d73e00645ebfe04a11bf26928
舒明恩 <wbshume@centaline.com.cn>
2017-10-09 18:08:14 +0800

    赎楼代办任务，部分数据需要在管理平台操作添加数据（例如添加菜单，分配权限等）

--- trade-web/src/main/java/com/centaline/trans/ransom/web/RansomDiscontinueController.java
+++ trade-web/src/main/java/com/centaline/trans/ransom/web/RansomDiscontinueController.java
@@ -1,25 +1,86 @@
 package com.centaline.trans.ransom.web;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.centaline.trans.engine.bean.RestVariable;
+import com.centaline.trans.engine.service.WorkFlowManager;
+import com.centaline.trans.ransom.entity.ToRansomDetailVo;
+import com.centaline.trans.ransom.service.RansomService;
+import com.centaline.trans.task.entity.ToApproveRecord;
+import com.centaline.trans.task.service.LoanlostApproveService;
+import com.centaline.trans.task.vo.LoanlostApproveVO;
+import com.centaline.trans.task.vo.ProcessInstanceVO;
 
 /**
  * <font color=red>流程引擎 任务处理</font>
  * @author wbwumf
  */
-@Controller("engine.ransomController")
-@RequestMapping("/engine/ransom")
+@Controller
+@RequestMapping("/ransomDiscontinue")
 public class RansomDiscontinueController {
+	
+	@Autowired
+	private WorkFlowManager workFlowManager;
+	
+	@Autowired(required=true)
+	private RansomService ransomService;
+	
+	@Autowired
+	private LoanlostApproveService loanlostApproveService;
 
-	@RequestMapping("{taskId}/process")
-	public String process(@PathVariable String taskId, String source, HttpServletRequest request,
+	@RequestMapping("/process")
+	public String process(String ransomCode, String taskId, String source, HttpServletRequest request,
 			HttpServletResponse response, RedirectAttributes attr){
-				
+		
+		ToRansomDetailVo detailVo = ransomService.getRansomDetail(ransomCode);
+		request.setAttribute("detailVo", detailVo);
+		
 		return null;
+	}
+	
+	@RequestMapping("/submitDiscontinue")
+	@ResponseBody
+	public boolean submitDiscontinue(HttpServletRequest request, ProcessInstanceVO processInstanceVO,
+			LoanlostApproveVO loanlostApproveVO){
+		ToApproveRecord toApproveRecord = saveToApproveRecord(processInstanceVO, loanlostApproveVO, "", "中止赎楼提交。");
+		
+		List<RestVariable> variables = new ArrayList<RestVariable>();
+		return workFlowManager.submitTask(variables, processInstanceVO.getTaskId(), processInstanceVO.getProcessInstanceId(), 
+				"toCase.getLeadingProcessId()", processInstanceVO.getCaseCode());
+	}
+	
+	/**
+	 * 保存审批记录
+	 * @param processInstanceVO
+	 * @param loanlostApproveVO
+	 * @param loanLost
+	 * @param loanLost_response
+	 * @return
+	 */
+	private ToApproveRecord saveToApproveRecord(ProcessInstanceVO processInstanceVO, LoanlostApproveVO loanlostApproveVO,
+			String loanLost, String loanLost_response) {
+		ToApproveRecord toApproveRecord = new ToApproveRecord();
+		toApproveRecord.setProcessInstance(processInstanceVO.getProcessInstanceId());
+		toApproveRecord.setPartCode(processInstanceVO.getPartCode());
+		toApproveRecord.setOperatorTime(new Date());
+		toApproveRecord.setApproveType(loanlostApproveVO.getApproveType());
+		toApproveRecord.setCaseCode(processInstanceVO.getCaseCode());
+		boolean b = loanLost.equals("true");
+		toApproveRecord.setContent((b?"通过":"不通过") + (",审批意见为"+loanLost_response));
+		toApproveRecord.setOperator(loanlostApproveVO.getOperator());
+		loanlostApproveService.saveLoanlostApprove(toApproveRecord);
+		return toApproveRecord;
 	}
 }
