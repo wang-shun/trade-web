1c654129941c6e363ba84fc94a4afb4ae85863a1
yinchao <yinchao@centaline.com.cn>
2017-10-10 14:34:18 +0800

    增加成交报告流程修改接口同步完成信息后，消息队列修改流程相关人员

--- trade-api/src/main/java/com/centaline/api/ccai/listener/FlowWorkListener.java
+++ trade-api/src/main/java/com/centaline/api/ccai/listener/FlowWorkListener.java
@@ -16,6 +16,7 @@ import com.centaline.trans.common.service.PropertyUtilsService;
 import com.centaline.trans.engine.WorkFlowConstant;
 import com.centaline.trans.engine.bean.ExecuteAction;
 import com.centaline.trans.engine.bean.ProcessInstance;
+import com.centaline.trans.engine.bean.RestVariable;
 import com.centaline.trans.engine.core.WorkFlowEngine;
 import com.centaline.trans.engine.entity.ToWorkFlow;
 import com.centaline.trans.engine.service.ToWorkFlowService;
@@ -35,6 +36,7 @@ import org.springframework.stereotype.Component;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.ObjectMessage;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -42,6 +44,7 @@ import java.util.Map;
 /**
  * 导入成功的成交报告信息消费者
  * 用于监听消息队列 启动交易主流程
+ *
  * @author yinchao
  * @date 2017/9/19
  */
@@ -71,16 +74,16 @@ public class FlowWorkListener {
 	private boolean enable;//是否自动启动流程
 
 	@JmsListener(destination = caseQueueName)
-	public void mqCaseOpertation(Message msg){
-		if(msg==null) return;
+	public void mqCaseOpertation(Message msg) {
+		if (msg == null) return;
 		try {
-			if(msg instanceof ObjectMessage){
+			if (msg instanceof ObjectMessage) {
 				ObjectMessage objectMessage = (ObjectMessage) msg;
 				//具体操作
 				caseOpertation((MQCaseMessage) objectMessage.getObject());
 			}
 		} catch (JMSException e) {
-			logger.error("change Message to Object error.",e);
+			logger.error("change Message to Object error.", e);
 		}
 	}
 
@@ -90,11 +93,12 @@ public class FlowWorkListener {
 
 	/**
 	 * 具体的操作
+	 *
 	 * @param message
 	 */
-	private void caseOpertation(MQCaseMessage message){
-		if(!enable){
-			logger.info("get a opertation:"+message+" but service is not enable.ignore");
+	private void caseOpertation(MQCaseMessage message) {
+		if (!enable) {
+			logger.info("get a opertation:" + message + " but service is not enable.ignore");
 			return;
 		}
 		MqOpertationLog mqlog = new MqOpertationLog();
@@ -105,18 +109,18 @@ public class FlowWorkListener {
 		mqlog.setCreateTime(new Date());
 		mqlog.setOpertationTime(new Date());
 		try {
-			if(MQCaseMessage.STARTFLOW_TYPE.equals(message.getType())){
+			if (MQCaseMessage.STARTFLOW_TYPE.equals(message.getType())) {
 				startProcess(message.getCaseCode());
 				mqlog.setStatus("0");
-			}else if(MQCaseMessage.UPDATEFLOW_TYPE.equals(message.getType())){
+			} else if (MQCaseMessage.UPDATEFLOW_TYPE.equals(message.getType())) {
 				updateProcess(message.getCaseCode());
 				mqlog.setStatus("0");
-			}else{
+			} else {
 				mqlog.setOpertation(message.getType());
 				mqlog.setStatus("-1");
 				mqlog.setErrmsg("未识别的操作.");
 			}
-		}catch (Exception e){
+		} catch (Exception e) {
 			mqlog.setStatus("-1");
 			mqlog.setErrmsg(e.getMessage());
 		}
@@ -126,35 +130,34 @@ public class FlowWorkListener {
 
 	/**
 	 * 开启案件交易流程
+	 *
 	 * @param caseCode
 	 * @throws BusinessException
 	 */
-	private void startProcess(String caseCode) throws BusinessException{
+	private void startProcess(String caseCode) throws BusinessException {
 		List<ToCaseParticipant> participants = participantMapper.selectByCaseCode(caseCode);
 		//流程引擎相关
 		Map<String, Object> defValsMap = propertyUtilsService.getProcessDefVals(WorkFlowEnum.WBUSSKEY.getCode());
 		ToCaseParticipant owner = null;
-		for(ToCaseParticipant pa : participants){
+		for (ToCaseParticipant pa : participants) {
 			//将所有的参与人 以参与人类型放入到流程引擎参数中
 			defValsMap.put(pa.getPosition(), pa.getUserName());
 			//有贷款权证 则贷款权证是案件拥有者 否则为过户权证
-			if(owner == null && CaseParticipantEnum.WARRANT.getCode().equals(pa.getPosition())){
+			if (owner == null && CaseParticipantEnum.WARRANT.getCode().equals(pa.getPosition())) {
 				owner = pa;
-			}else if(CaseParticipantEnum.LOAN.getCode().equals(pa.getPosition())){
+			} else if (CaseParticipantEnum.LOAN.getCode().equals(pa.getPosition())) {
 				owner = pa;
 			}
 		}
-		if(owner!=null){
+		if (owner != null) {
 			//设置流程引擎平台登录用户 否则无法创建httpclient
 			engine.setAuthUserName(owner.getUserName());
 			//接单跟进人员
-			defValsMap.put("receiver",owner.getUserName());
+			defValsMap.put("receiver", owner.getUserName());
 			//权证经理环节
-			defValsMap.put("manager",owner.getGrpMgrUsername());
-			//TODO 兼容原有流程 否则会无法启动 后面正式流程删除
-			defValsMap.put("caseOwner",owner.getGrpMgrUsername());
-		}else{
-			throw new BusinessException("交易案件 编号["+caseCode+"] 未获取到案件贷款或过户权证信息 启动流程失败.");
+			defValsMap.put("manager", owner.getGrpMgrUsername());
+		} else {
+			throw new BusinessException("交易案件 编号[" + caseCode + "] 未获取到案件贷款或过户权证信息 启动流程失败.");
 		}
 
 		ToWorkFlow toWorkFlow = new ToWorkFlow();
@@ -171,17 +174,18 @@ public class FlowWorkListener {
 
 		toWorkFlowService.insertSelective(toWorkFlow);
 
-		logger.info("交易案件 编号["+caseCode+"] 主流程启动成功");
+		logger.info("交易案件 编号[" + caseCode + "] 主流程启动成功");
 	}
 
 	/**
 	 * 流程启动
 	 * 主要用于在启动流程前设置用户 否则httpclien会报控制空指针 复制原方法见see
-	 * @see com.centaline.trans.engine.service.impl.ProcessInstanceServiceImpl#startWorkFlowByDfId(String, String, Map)
+	 *
 	 * @param processDefinitionId
 	 * @param businessKey
 	 * @param vars
 	 * @return
+	 * @see com.centaline.trans.engine.service.impl.ProcessInstanceServiceImpl#startWorkFlowByDfId(String, String, Map)
 	 */
 	private StartProcessInstanceVo startWorkFlowBase(String processDefinitionId, String businessKey, Map<String, Object> vars) {
 		ProcessInstance process = new ProcessInstance();
@@ -196,15 +200,16 @@ public class FlowWorkListener {
 	/**
 	 * 向流程发送CCAI修改完成消息
 	 * 并更改案件状态
+	 *
 	 * @param caseCode
 	 */
-	private void updateProcess(String caseCode){
+	private void updateProcess(String caseCode) {
 		//获取流程信息
 		ToWorkFlow wf = new ToWorkFlow();
 		wf.setCaseCode(caseCode);
 		wf.setBusinessKey(WorkFlowEnum.WBUSSKEY.getCode());
 		ToWorkFlow wordkFlowDB = toWorkFlowService.queryActiveToWorkFlowByCaseCodeBusKey(wf);
-		if(wordkFlowDB!=null){
+		if (wordkFlowDB != null) {
 			// 发送消息
 			ActRuEventSubScr event = new ActRuEventSubScr();
 			event.setEventType(MessageEnum.CCAI_UPDATED_MSG.getEventType());
@@ -214,11 +219,15 @@ public class FlowWorkListener {
 			ExecuteAction action = new ExecuteAction();
 			action.setAction(EventTypeEnum.CCAI_UPDATED_MSG_EVENT_CATCH.getEventType());
 			action.setMessageName(MessageEnum.CCAI_UPDATED_MSG.getName());
-			List<ActRuEventSubScr> subScrs= actRuEventSubScrMapper.listBySelective(event);
-			if(CollectionUtils.isNotEmpty(subScrs)) {
+			List<ActRuEventSubScr> subScrs = actRuEventSubScrMapper.listBySelective(event);
+			if (CollectionUtils.isNotEmpty(subScrs)) {
 				//设置流程引擎登录用户 否则无法访问REST接口
 				User user = uamUserOrgService.getUserById(wordkFlowDB.getProcessOwner());
 				engine.setAuthUserName(user.getUsername());
+
+				//更新案件参与人信息 及 案件所有人信息
+				action.setVariables(updateFlowParticipants(caseCode, wordkFlowDB.getPkid()));
+
 				//调用REST接口发送消息
 				action.setExecutionId(subScrs.get(0).getExecutionId());
 				engine.RESTfulWorkFlow(WorkFlowConstant.PUT_EXECUTE_KEY, ExecutionVo.class, action);
@@ -230,5 +239,35 @@ public class FlowWorkListener {
 		}
 	}
 
+	/**
+	 * 更新案件流程相关的任务环节人员信息
+	 *
+	 * @param caseCode
+	 * @return 返回最新的人员参数设置
+	 */
+	private List<RestVariable> updateFlowParticipants(String caseCode, Long toWorkFlowId) {
+		List<ToCaseParticipant> participants = participantMapper.selectByCaseCode(caseCode);
+		List<RestVariable> variables = new ArrayList<>();
+		ToCaseParticipant owner = null;
+		//更新流程设置的案件人员信息
+		for (ToCaseParticipant pa : participants) {
+			variables.add(new RestVariable(pa.getPosition(), pa.getUserName()));
+			//有贷款权证 则贷款权证是案件拥有者 否则为过户权证
+			if (owner == null && CaseParticipantEnum.WARRANT.getCode().equals(pa.getPosition())) {
+				owner = pa;
+			} else if (CaseParticipantEnum.LOAN.getCode().equals(pa.getPosition())) {
+				owner = pa;
+			}
+		}
+		variables.add(new RestVariable("receiver", owner.getUserName()));
+		variables.add(new RestVariable("manager", owner.getGrpMgrUsername()));
+		ToWorkFlow toWorkFlow = new ToWorkFlow();
+		toWorkFlow.setPkid(toWorkFlowId);
+		//更新案件拥有人信息
+		User user = uamUserOrgService.getUserByUsername(owner.getUserName());
+		toWorkFlow.setProcessOwner(user.getId());
+		toWorkFlowService.updateByPrimaryKeySelective(toWorkFlow);
+		return variables;
+	}
 
 }
