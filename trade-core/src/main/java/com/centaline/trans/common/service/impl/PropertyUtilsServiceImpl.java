07972c7e150915b7bce5f9dd2665097d508397cb
CENTALINE\xiefei1 <xiefei1@centaline.com.cn>
2017-10-11 10:11:53 +0800

    propertyUtilsServiceimpl报错调试

--- trade-core/src/main/java/com/centaline/trans/common/service/impl/PropertyUtilsServiceImpl.java
+++ trade-core/src/main/java/com/centaline/trans/common/service/impl/PropertyUtilsServiceImpl.java
@@ -33,8 +33,8 @@ public class PropertyUtilsServiceImpl implements PropertyUtilsService {
 	@Value("${process.df.key.spv_out}")
 	private String spvOut;
 
-	@Value("${process.df.key.Ransom_Process}")
-	private String processRansomKey;
+//	@Value("${process.df.key.Ransom_Process}")
+//	private String processRansomKey;
 	
 	@Value("${process.df.key.operation_process}")
 	private String processOperationDfKey;
