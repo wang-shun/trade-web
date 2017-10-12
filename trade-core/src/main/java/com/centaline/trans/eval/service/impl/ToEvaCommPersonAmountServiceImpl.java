4a2f6f08c0c7ecbf2e071823e74abc67b4f30911
CENTALINE\xiefei1 <xiefei1@centaline.com.cn>
2017-10-12 11:15:29 +0800

    评估调佣对象，调佣金额，本地提交

---
+++ trade-core/src/main/java/com/centaline/trans/eval/service/impl/ToEvaCommPersonAmountServiceImpl.java
@@ -0,0 +1,114 @@
+package com.centaline.trans.eval.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aist.common.exception.BusinessException;
+import com.centaline.trans.eval.entity.ToEvaCommPersonAmount;
+import com.centaline.trans.eval.entity.ToEvaCommissionChange;
+import com.centaline.trans.eval.repository.ToEvaCommPersonAmountMapper;
+import com.centaline.trans.eval.repository.ToEvaCommissionChangeMapper;
+import com.centaline.trans.eval.service.ToEvaCommPersonAmountService;
+import com.centaline.trans.eval.vo.EvalChangeCommVO;
+/**
+ * @author xiefei1
+ * @since 2017年10月11日 下午3:14:04 
+ * @description 调佣对象与调佣金额
+ */
+
+@Service
+public class ToEvaCommPersonAmountServiceImpl implements ToEvaCommPersonAmountService {
+	@Autowired
+	private ToEvaCommPersonAmountMapper toEvaCommPersonAmountMapper; 
+	@Autowired
+	private ToEvaCommissionChangeMapper toEvaCommissionChangeMapper; 
+	@Override
+	public int deleteByPrimaryKey(Long pkid) {
+		// TODO Auto-generated method stub
+		return toEvaCommPersonAmountMapper.deleteByPrimaryKey(pkid);
+	}
+
+	@Override
+	public int insert(ToEvaCommPersonAmount record) {
+		// TODO Auto-generated method stub
+		return toEvaCommPersonAmountMapper.insert(record);
+	}
+
+	@Override
+	public int insertSelective(ToEvaCommPersonAmount record) {
+		// TODO Auto-generated method stub
+		return toEvaCommPersonAmountMapper.insertSelective(record);
+	}
+
+	@Override
+	public ToEvaCommPersonAmount selectByPrimaryKey(Long pkid) {
+		// TODO Auto-generated method stub
+		return toEvaCommPersonAmountMapper.selectByPrimaryKey(pkid);
+	}
+
+	@Override
+	public int updateByPrimaryKeySelective(ToEvaCommPersonAmount record) {
+		// TODO Auto-generated method stub
+		return toEvaCommPersonAmountMapper.updateByPrimaryKeySelective(record);
+	}
+
+	@Override
+	public int updateByPrimaryKey(ToEvaCommPersonAmount record) {
+		// TODO Auto-generated method stub
+		return toEvaCommPersonAmountMapper.updateByPrimaryKey(record);
+	}
+//	获取完整的调佣对象与调佣金额VO
+	@Override
+	public EvalChangeCommVO getFullEvalChangeCommVO(String caseCode) {
+		if(caseCode==null||caseCode==""){
+			throw new BusinessException("caseCode案件编码为空，请检查！");
+		}
+		EvalChangeCommVO evalChangeCommVO = new EvalChangeCommVO(caseCode);
+		List<ToEvaCommPersonAmount> ToEvaCommPersonAmountList = toEvaCommPersonAmountMapper.selectByCasecode(caseCode);
+		for (ToEvaCommPersonAmount toEvaCommPersonAmount : ToEvaCommPersonAmountList) {
+			if(toEvaCommPersonAmount.getPosition().contains("权证")){
+				evalChangeCommVO.getWarrantPersonList().add(toEvaCommPersonAmount);
+			}else if(toEvaCommPersonAmount.getPosition().contains("分成")){
+				evalChangeCommVO.getSharePersonList().add(toEvaCommPersonAmount);
+			}else{
+				evalChangeCommVO.getCoPersonList().add(toEvaCommPersonAmount);
+			}
+		}
+		ToEvaCommissionChange toEvaCommissionChange = toEvaCommissionChangeMapper.selectByCaseCode(caseCode);
+		evalChangeCommVO.setTtlComm(toEvaCommissionChange.getCommisionTtlAmount());
+		return evalChangeCommVO;
+	}
+//	保存调佣对象与调佣金额VO
+//	save**是有事务的
+	@Override
+	public void saveEvalChangeCommVO(EvalChangeCommVO evalChangeCommVO) {
+		ArrayList<ToEvaCommPersonAmount> toEvaCommPersonAmountList = new ArrayList<ToEvaCommPersonAmount>();
+
+		List<ToEvaCommPersonAmount> coPersonList = evalChangeCommVO.getCoPersonList();
+		for (ToEvaCommPersonAmount toEvaCommPersonAmount : coPersonList) {
+			toEvaCommPersonAmountList.add(toEvaCommPersonAmount);
+		}
+		List<ToEvaCommPersonAmount> sharePersonList = evalChangeCommVO.getSharePersonList();
+		for (ToEvaCommPersonAmount toEvaCommPersonAmount : sharePersonList) {
+			toEvaCommPersonAmountList.add(toEvaCommPersonAmount);
+		}
+		List<ToEvaCommPersonAmount> warrantPersonList = evalChangeCommVO.getWarrantPersonList();
+		for (ToEvaCommPersonAmount toEvaCommPersonAmount : warrantPersonList) {
+			toEvaCommPersonAmountList.add(toEvaCommPersonAmount);
+		}
+		
+		for (ToEvaCommPersonAmount toEvaCommPersonAmount : toEvaCommPersonAmountList) {
+			toEvaCommPersonAmountMapper.updateByCaseCodeSelective(toEvaCommPersonAmount);
+		}
+		if(null!=evalChangeCommVO.getTtlComm()){
+			ToEvaCommissionChange toEvaCommissionChange = new ToEvaCommissionChange();
+			toEvaCommissionChange.setCommisionTtlAmount(evalChangeCommVO.getTtlComm());
+			toEvaCommissionChangeMapper.updateByCaseCodeSelective(toEvaCommissionChange);			
+		}
+
+	}
+
+}
