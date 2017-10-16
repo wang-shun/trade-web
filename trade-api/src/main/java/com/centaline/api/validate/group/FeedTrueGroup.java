7fb09f89d896017accbdbc1a5e078fe8d08fcac3
yinchao <yinchao@centaline.com.cn>
2017-10-16 20:13:30 +0800

    增加银行返利，财务审批反馈接口

---
+++ trade-api/src/main/java/com/centaline/api/validate/group/FeedTrueGroup.java
@@ -0,0 +1,10 @@
+package com.centaline.api.validate.group;
+
+/**
+ * 用于注解标记财务审批返回成功的校验
+ * 默认为@see javax.validation.groups.Default组别，用于与default区别
+ * @author yinchao
+ * @date 2017/9/11
+ */
+public interface FeedTrueGroup {
+}
