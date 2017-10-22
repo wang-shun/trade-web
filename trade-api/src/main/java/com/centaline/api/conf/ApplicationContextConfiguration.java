21468ada21992fd09ac3524817c0cf643b12b818
邬杰 <wujie18@centaline.com.cn>
2017-10-22 19:39:56 +0800

    去掉读取配置信息

--- trade-api/src/main/java/com/centaline/api/conf/ApplicationContextConfiguration.java
+++ trade-api/src/main/java/com/centaline/api/conf/ApplicationContextConfiguration.java
@@ -11,26 +11,6 @@ import org.springframework.core.io.ClassPathResource;
 public class ApplicationContextConfiguration {
 
 	@Bean
-	@Profile("dev")
-	public PropertySourcesPlaceholderConfigurer createPropertySourcesPlaceholderConfigurerDev() {
-		ClassPathResource resource = new ClassPathResource("application-dev.properties");
-		PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-		propertyPlaceholderConfigurer.setLocation(resource);
-		propertyPlaceholderConfigurer.setIgnoreUnresolvablePlaceholders(true);
-		return propertyPlaceholderConfigurer;
-	}
-
-	@Bean
-	@Profile("production")
-	public PropertySourcesPlaceholderConfigurer createPropertySourcesPlaceholderConfigurerPrd() {
-		ClassPathResource resource = new ClassPathResource("application-prod.properties");
-		PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-		propertyPlaceholderConfigurer.setLocation(resource);
-		propertyPlaceholderConfigurer.setIgnoreUnresolvablePlaceholders(true);
-		return propertyPlaceholderConfigurer;
-	}
-
-	@Bean
 	public FilterRegistrationBean loggingFilterRegistration() {
 		FilterRegistrationBean registration = new FilterRegistrationBean(new com.github.isrsal.logging.LoggingFilter());
 		registration.addUrlPatterns("/api/*");//只记录api的接口信息 否则日志太多
